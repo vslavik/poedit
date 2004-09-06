5b94c799a654a27d1360ef1b11577b5a5decf355
Vaclav Slavik <vslavik@fastmail.fm>
2004-09-06 11:21:09 +0000

    compilation fix

--- poedit/src/chooselang.cpp
+++ poedit/src/chooselang.cpp
@@ -101,7 +101,7 @@ wxLanguage ChooseLanguage()
         { _T("Lithuanian"), wxLANGUAGE_LITHUANIAN },
         { _T("Mongolian"), wxLANGUAGE_MONGOLIAN },
         { _T("Norwegian Nynorsk"), wxLANGUAGE_NORWEGIAN_NYNORSK },
-        { _T("Norwegian Bokmål"), wxLANGUAGE_NORWEGIAN_BOKMAL },
+        { _T("Norwegian Bokmal"), wxLANGUAGE_NORWEGIAN_BOKMAL },
         { _T("Polish"), wxLANGUAGE_POLISH },
         { _T("Portuguese"), wxLANGUAGE_PORTUGUESE },
         { _T("Portuguese (Brazilian)"), wxLANGUAGE_PORTUGUESE_BRAZILIAN },
