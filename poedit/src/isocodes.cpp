cc0f2e5511c07843e3cec21e8ca66441059df592
Vaclav Slavik <vslavik@fastmail.fm>
2005-06-19 16:30:17 +0000

    added headers with license information

--- poedit/src/isocodes.cpp
+++ poedit/src/isocodes.cpp
@@ -1,3 +1,32 @@
+/*
+ *  This file is part of poEdit (http: *www.poedit.org)
+ *
+ *  Copyright (C) 1999-2005 Vaclav Slavik
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *  $Id$
+ *
+ *  This file contains languages & countries names and 2-letter codes as
+ *  defined by ISO 639 and ISO 3166 standards.
+ *
+ */
 
 #include <wx/wxprec.h>
 
@@ -195,7 +224,7 @@ LanguageStruct isoLanguages[] =
     {_T("za"), _T("Zhuang")},
     {_T("zh"), _T("Chinese")},
     {_T("zu"), _T("Zulu")},
-    
+
     {NULL, NULL}
 };
 
@@ -444,7 +473,7 @@ LanguageStruct isoCountries[] =
     {_T("YU"), _T("YUGOSLAVIA")},
     {_T("ZM"), _T("ZAMBIA")},
     {_T("ZW"), _T("ZIMBABWE")},
-    
+
     {NULL, NULL}
 };
 
--- poedit/src/summarydlg.cpp
+++ poedit/src/summarydlg.cpp
@@ -1,15 +1,31 @@
 /*
-
-    poedit, a wxWindows i18n catalogs editor
-
-    ---------------
-      summarydlg.cpp
-    
-      Catalog update summary dialog
-    
-      (c) Vaclav Slavik, 2000
-
-*/
+ *  This file is part of poEdit (http: *www.poedit.org)
+ *
+ *  Copyright (C) 2000-2005 Vaclav Slavik
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *  $Id$
+ *
+ *  Catalog update summary dialog
+ *
+ */
 
 #include <wx/wxprec.h>
 
