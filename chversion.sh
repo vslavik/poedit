b5b98eed8008e44bd9a0558f90a60faaf4ebaa8a
Vaclav Slavik <vslavik@fastmail.fm>
2007-11-22 12:51:27 +0000

    added Project-Id-Version in *.po files to chkversion.sh

--- chversion.sh
+++ chversion.sh
@@ -20,3 +20,8 @@ replace_ver make-distrib.sh \
             '\(VERSION=\).*' "\1$VER"
 replace_ver src/edapp.cpp \
             '\(wxString version(_T("\).*\("));\)' "\1$VER\2"
+
+for i in locales/*.po ; do
+    replace_ver $i \
+                '\(Project-Id-Version:\)[^\\]*' "\1 Poedit $VER"
+done
