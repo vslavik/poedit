25e35d7132802c37148173a13a0652e1b057c19b
Vaclav Slavik <vslavik@fastmail.fm>
2007-11-24 16:32:49 +0000

    handle pre-releases version numbers

--- chversion.sh
+++ chversion.sh
@@ -10,18 +10,19 @@ q
 " | ed -s $1 2>/dev/null
 }
 
-VER=$1
+VER_FULL=$1
+VER_SHORT=`echo $VER_FULL | sed -e 's/pre[0-9]//g'`
 
 replace_ver win32/poedit.iss \
-            '\(#define VERSION *"\).*\("\)' "\1$VER\2"
+            '\(#define VERSION *"\).*\("\)' "\1$VER_FULL\2"
 replace_ver configure.in \
-            '\(AC_INIT(\[poedit\], \[\)[^]]*\(\],.*\)' "\1$VER\2"
+            '\(AC_INIT(\[poedit\], \[\)[^]]*\(\],.*\)' "\1$VER_FULL\2"
 replace_ver make-distrib.sh \
-            '\(VERSION=\).*' "\1$VER"
+            '\(VERSION=\).*' "\1$VER_FULL"
 replace_ver src/edapp.cpp \
-            '\(wxString version(_T("\).*\("));\)' "\1$VER\2"
+            '\(wxString version(_T("\).*\("));\)' "\1$VER_SHORT\2"
 
 for i in locales/*.po ; do
     replace_ver $i \
-                '\(Project-Id-Version:\)[^\\]*' "\1 Poedit $VER"
+                '\(Project-Id-Version:\)[^\\]*' "\1 Poedit $VER_SHORT"
 done
