558823a56c2dc23906ea1d339c94dd49b03490b9
Vaclav Slavik <vslavik@fastmail.fm>
2008-02-17 16:13:11 +0000

    OS X: fixed conflict with Spaces by allowing Cmd-arrows to be used for list control navigation in addition to Ctrl-arrows

--- src/edframe.cpp
+++ src/edframe.cpp
@@ -164,7 +164,7 @@ class TextctrlHandler : public wxEvtHandler
         {
             int keyCode = event.GetKeyCode();
 
-            if (!event.ControlDown())
+            if (!(event.ControlDown() || event.CmdDown()))
             {
                 event.Skip();
                 return;
