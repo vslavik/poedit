d9c13da6a5eac51daed2f8eec9c9c136f5d1c169
Vaclav Slavik <vslavik@fastmail.fm>
2008-03-15 14:50:58 +0000

    fixed HTML export to properly escape the text (#1739062)

--- src/export_html.cpp
+++ src/export_html.cpp
@@ -34,6 +34,9 @@
 
 #include "catalog.h"
 
+namespace
+{
+
 // colours used in the list:
 // (FIXME: this is duplicated with code in edframe.cpp, get rid of this
 // duplication, preferably by making the colours customizable and stored
@@ -44,12 +47,23 @@
                                        int((g)*g_darkColourFactor),\
                                        int((b)*g_darkColourFactor)))
 #define LIST_COLOURS(r,g,b) { wxColour(r,g,b), DARKEN_COLOUR(r,g,b) }
-static wxColour
+wxColour
     g_ItemColourNormal[2] =       LIST_COLOURS(0xFF,0xFF,0xFF), // white
     g_ItemColourUntranslated[2] = LIST_COLOURS(0xA5,0xEA,0xEF), // blue
     g_ItemColourFuzzy[2] =        LIST_COLOURS(0xF4,0xF1,0xC1); // yellow
 
 
+// escape string for HTML:
+wxString Escape(const wxString& str)
+{
+    wxString s(str);
+    s.Replace(_T("&"), _T("&amp;"));
+    s.Replace(_T("<"), _T("&lt;"));
+    s.Replace(_T(">"), _T("&gt;"));
+    return s;
+}
+
+} // anonymous namespace
 
 bool Catalog::ExportToHTML(const wxString& filename)
 {
@@ -76,18 +90,18 @@ bool Catalog::ExportToHTML(const wxString& filename)
 
     f.AddLine(_T("<head>"));
     line.Printf(_T("<title> %s - %s / %s - Poedit Export </title>"),
-                m_header.Project.c_str(),
-                m_header.Language.c_str(),
-                m_header.Country.c_str());
+                Escape(m_header.Project).c_str(),
+                Escape(m_header.Language).c_str(),
+                Escape(m_header.Country).c_str());
     f.AddLine(line);
     f.AddLine(_T("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" ) );
     f.AddLine(_T("</head>"));
     f.AddLine(_T("<body bgcolor='#FFFFFF'>"));
 
     line.Printf(_T("<h1> %s : %s / %s</h1>"),
-               m_header.Project.c_str(),
-               m_header.Language.c_str(),
-               m_header.Country.c_str());
+                Escape(m_header.Project).c_str(),
+                Escape(m_header.Language).c_str(),
+                Escape(m_header.Country).c_str());
     f.AddLine(line);
 
 
@@ -104,23 +118,24 @@ bool Catalog::ExportToHTML(const wxString& filename)
     wxString line_format = _T("<tr><td>%s</td><td>%s</td></tr>");
     line.Printf(line_format,
                 _("Project name and version:"),
-                m_header.Project.c_str());
+                Escape(m_header.Project).c_str());
     f.AddLine(line);
     line.Printf(line_format, _("Language:"),
-                m_header.Language.c_str());
+                Escape(m_header.Language).c_str());
     f.AddLine(line);
     line.Printf(line_format, _("Country:"),
-                m_header.Country.c_str());
+                Escape(m_header.Country).c_str());
     f.AddLine(line);
     line.Printf(line_format, _("Team:"),
-                m_header.Team.c_str());
+                Escape(m_header.Team).c_str());
     f.AddLine(line);
     line.Printf(_T("<tr><td>%s</td><td><a href=\"mailto:%s\">%s</a></td></tr>"),
                 _("Team's email address:"),
-                m_header.TeamEmail.c_str(), m_header.TeamEmail.c_str());
+                Escape(m_header.TeamEmail).c_str(),
+                Escape(m_header.TeamEmail).c_str());
     f.AddLine(line);
     line.Printf(line_format, _("Charset:"),
-                m_header.Charset.c_str());
+                Escape(m_header.Charset).c_str());
     f.AddLine(line);
 
     f.AddLine( _T("</table>") );
@@ -155,7 +170,7 @@ bool Catalog::ExportToHTML(const wxString& filename)
     f.AddLine(_("Notes"));
     f.AddLine(_T("</th>"));
     f.AddLine(_T("</tr>"));
-    
+
     for (i = 0; i < GetCount(); i++)
     {
         const CatalogItem& data = m_items[i];
@@ -166,7 +181,7 @@ bool Catalog::ExportToHTML(const wxString& filename)
         wxString translation = data.GetTranslation();
         if (translation.empty())
         {
-            translation = _T("&nbsp;");
+            translation = _T(" ");
             bgcolor = g_ItemColourUntranslated[i % 2];
         }
 
@@ -185,23 +200,23 @@ bool Catalog::ExportToHTML(const wxString& filename)
         }
         if (flags.empty())
         {
-            flags = _T("&nbsp;");
+            flags = _T(" ");
         }
-        
+
         wxString tr;
         tr.Printf(_T("<tr bgcolor='#%0X%0X%0X'>"),
                   bgcolor.Red(), bgcolor.Green(), bgcolor.Blue());
         f.AddLine(tr);
 
         f.AddLine(_T("<td>"));
-        f.AddLine(original_string);
+        f.AddLine(Escape(original_string));
         f.AddLine(_T("</td>"));
         f.AddLine(_T("<td>"));
-        f.AddLine(translation);
+        f.AddLine(Escape(translation));
         f.AddLine(_T("</td>"));
         f.AddLine(_T("<td>"));
         f.AddLine(_T("<font size=\"-1\">"));
-        f.AddLine(flags);
+        f.AddLine(Escape(flags));
         f.AddLine(_T("</font>"));
         f.AddLine(_T("</td>"));
         f.AddLine(_T("</tr>"));
