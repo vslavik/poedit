2a62de467d35b9b52ff84678af1c9d4016364664
Vaclav Slavik <vslavik@fastmail.fm>
2010-01-22 08:06:12 +0100

    Recommend use of UTF-8 charset.
    Also, don't remember previously used charsets, it does more harm than
    it's useful for (see bug #335).

--- src/settingsdlg.cpp
+++ src/settingsdlg.cpp
@@ -80,28 +80,69 @@ SettingsDialog::SettingsDialog(wxWindow *parent)
 }
 
 
-#define DEFAULT_CHARSETS \
-    _T(":utf-8:iso-8859-1:iso-8859-2:iso-8859-3:iso-8859-4:iso-8859-5")\
-    _T(":iso-8859-6:iso-8859-7:iso-8859-8:iso-8859-9:iso-8859-10")\
-    _T(":iso-8859-11:iso-8859-12:iso-8859-13:iso-8859-14:iso-8859-15:")\
-    _T(":koi8-r:windows-1250:windows-1251:windows-1252:windows-1253:")\
-    _T(":windows-1254:windows-1255:windows-1256:windows-1257:")
+namespace
+{
+
+#define UTF_8_CHARSET  _("UTF-8 (recommended)")
+
+void SetCharsetToCombobox(wxComboBox *ctrl, const wxString& value)
+{
+    static const wxString all_charsets[] =
+        {
+        UTF_8_CHARSET,
+        // and legacy ones
+        _T("iso-8859-1"),
+        _T("iso-8859-2"),
+        _T("iso-8859-3"),
+        _T("iso-8859-4"),
+        _T("iso-8859-5"),
+        _T("iso-8859-6"),
+        _T("iso-8859-7"),
+        _T("iso-8859-8"),
+        _T("iso-8859-9"),
+        _T("iso-8859-10"),
+        _T("iso-8859-11"),
+        _T("iso-8859-12"),
+        _T("iso-8859-13"),
+        _T("iso-8859-14"),
+        _T("iso-8859-15"),
+        _T("koi8-r"),
+        _T("windows-1250"),
+        _T("windows-1251"),
+        _T("windows-1252"),
+        _T("windows-1253"),
+        _T("windows-1254"),
+        _T("windows-1255"),
+        _T("windows-1256"),
+        _T("windows-1257")
+        };
+
+    ctrl->Clear();
+    for ( int i = 0; i < WXSIZEOF(all_charsets); i++ )
+        ctrl->Append(all_charsets[i]);
+
+    const wxString low = value.Lower();
+    if ( low == _T("utf-8") || low == _T("utf8") )
+        ctrl->SetValue(UTF_8_CHARSET);
+    else
+        ctrl->SetValue(value);
+}
+
+wxString GetCharsetFromCombobox(wxComboBox *ctrl)
+{
+    wxString c = ctrl->GetValue();
+    if ( c == UTF_8_CHARSET )
+        c = _T("UTF-8");
+    return c;
+}
+
+} // anonymous namespace
+
 
 void SettingsDialog::TransferTo(Catalog *cat)
 {
-    wxStringTokenizer tkn(wxConfig::Get()->Read(_T("used_charsets"), 
-                          DEFAULT_CHARSETS), _T(":"), wxTOKEN_STRTOK);
-
-    while (tkn.HasMoreTokens())
-    {
-        wxString strToken = tkn.GetNextToken();
-        m_charset->Append(strToken);
-
-        // check if the charset is supported by iconv()
-        if(strToken.compare(0, 3, _T("utf")) == 0 ||
-                           strToken.compare(0, 3, _T("iso")) == 0)
-            m_sourceCodeCharset->Append(strToken);
-    }
+    SetCharsetToCombobox(m_charset, cat->Header().Charset);
+    SetCharsetToCombobox(m_sourceCodeCharset, cat->Header().SourceCodeCharset);
 
     #define SET_VAL(what,what2) m_##what2->SetValue(cat->Header().what)
     SET_VAL(Team, team);
@@ -110,35 +151,33 @@ void SettingsDialog::TransferTo(Catalog *cat)
     SET_VAL(BasePath, basePath);
     SET_VAL(Language, language);
     SET_VAL(Country, country);
-    SET_VAL(Charset, charset);
-    SET_VAL(SourceCodeCharset, sourceCodeCharset);
     #undef SET_VAL
 
     if (cat->Header().HasHeader(_T("Plural-Forms")))
         m_pluralForms->SetValue(cat->Header().GetHeader(_T("Plural-Forms")));
-    
+
     m_paths->SetStrings(cat->Header().SearchPaths);
     m_keywords->SetStrings(cat->Header().Keywords);
 }
 
- 
-            
+
 void SettingsDialog::TransferFrom(Catalog *cat)
 {
+    cat->Header().Charset = GetCharsetFromCombobox(m_charset);
+    cat->Header().SourceCodeCharset = GetCharsetFromCombobox(m_sourceCodeCharset);
+
     #define GET_VAL(what,what2) cat->Header().what = m_##what2->GetValue()
     GET_VAL(Language, language);
     GET_VAL(Country, country);
-    GET_VAL(Charset, charset);
     GET_VAL(Team, team);
     GET_VAL(TeamEmail, teamEmail);
     GET_VAL(Project, project);
     GET_VAL(BasePath, basePath);
-    GET_VAL(SourceCodeCharset, sourceCodeCharset);
     #undef GET_VAL
 
     wxString dummy;
-    wxArrayString arr;   
-    
+    wxArrayString arr;
+
     cat->Header().SearchPaths.Clear();
     cat->Header().Keywords.Clear();
 
@@ -157,14 +196,6 @@ void SettingsDialog::TransferFrom(Catalog *cat)
     m_keywords->GetStrings(arr);
     cat->Header().Keywords = arr;
 
-    wxString charsets = wxConfig::Get()->Read(_T("used_charsets"), DEFAULT_CHARSETS);
-    wxString charset = m_charset->GetValue().Lower();
-    if (!charsets.Contains(_T(":")+charset+_T(":")))
-    {
-        charsets = _T(":") + charset + charsets;
-        wxConfig::Get()->Write(_T("used_charsets"), charsets);
-    }
-   
     cat->Header().SetHeaderNotEmpty(_T("Plural-Forms"),
                                     m_pluralForms->GetValue());
 }
