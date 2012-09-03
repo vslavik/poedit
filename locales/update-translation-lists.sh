9679ac6332a845304ba77732f577f56fe6403049
Vaclav Slavik <vslavik@gmail.com>
2012-09-03 15:05:21 +0200

    Add CFBundleLocalizations to Info.plist.
    This is required for Cocoa to understand what localizations there are.
    Even though wxWidgets library works without it, e.g. Sparkle won't show
    non-English UI unless [one of] the user's preferred language is listed
    in CFBundleLocalizations.

--- locales/update-translation-lists.sh
+++ locales/update-translation-lists.sh
@@ -32,10 +32,30 @@ for i in list:
 
 update_makefile_am()
 {
-    echo updating Makefile.am...
+    echo "updating Makefile.am..."
     replace_str Makefile.am \
             '\(POEDIT_LINGUAS = \).*' "\1$LANGS_POEDIT"
 }
 
 
+INFO_PLIST=../macosx/Info.plist.in
+
+generate_info_plist()
+{
+    sed -n -e '1,/begin localizations list/ p' $INFO_PLIST
+    for lang in $LANGS_POEDIT ; do
+        echo "        <string>$lang</string>"
+    done
+    sed -n -e '/end localizations list/,$ p' $INFO_PLIST
+}
+
+update_info_plist()
+{
+    echo "updating Info.plist.in..."
+    generate_info_plist >$INFO_PLIST.new
+    mv -f $INFO_PLIST.new $INFO_PLIST
+}
+
+
 update_makefile_am
+update_info_plist
