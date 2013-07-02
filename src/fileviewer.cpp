fce4f6506afbb4f90178f5884690a023308da9c6
Vaclav Slavik <vslavik@gmail.com>
2013-07-02 10:16:23 +0200

    Try harder when finding source files, look in parent dirs too.
    Sometimes, the path in source reference is not relative to the PO file's
    location, but is relative to e.g. the root directory. See
    https://code.djangoproject.com/ticket/13936 for exhaustive discussion
    with plenty of examples.
    
    Deal with this by trying parent directories of m_basePath too. So if a
    file named project/locales/cs/foo.po has a reference to src/main.c, try
    not only project/locales/cs/src/main.c, but also
    project/locales/src/main.c and project/src/main.c etc

--- src/fileviewer.cpp
+++ src/fileviewer.cpp
@@ -229,7 +229,40 @@ void FileViewer::ShowReference(wxString ref)
 
     wxPathFormat pathfmt = ref.Contains(_T('\\')) ? wxPATH_WIN : wxPATH_UNIX;
     wxFileName filename(ref.BeforeLast(_T(':')), pathfmt);
-    filename.MakeAbsolute(m_basePath);
+
+    if ( filename.IsRelative() )
+    {
+        wxFileName relative(filename);
+        wxString basePath(m_basePath);
+
+        // Sometimes, the path in source reference is not relative to the PO
+        // file's location, but is relative to e.g. the root directory. See
+        // https://code.djangoproject.com/ticket/13936 for exhaustive
+        // discussion with plenty of examples.
+        //
+        // Deal with this by trying parent directories of m_basePath too. So if
+        // a file named project/locales/cs/foo.po has a reference to src/main.c,
+        // try not only project/locales/cs/src/main.c, but also
+        // project/locales/src/main.c and project/src/main.c etc.
+        while ( !basePath.empty() )
+        {
+            filename = relative;
+            filename.MakeAbsolute(basePath);
+            if ( filename.FileExists() )
+            {
+                break; // good, found the file
+            }
+            else
+            {
+                // remove the last path component
+                size_t last = basePath.find_last_of(_T("\\/"));
+                if ( last == wxString::npos )
+                    break;
+                else
+                    basePath.erase(last);
+            }
+        }
+    }
 
     wxFFile file;
     wxString data;
