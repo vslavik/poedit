e2b0e65549fb6e250f99eb5daf7ae173543f14ae
Vaclav Slavik <vslavik@gmail.com>
2013-09-07 15:39:36 +0200

    Import gettext-0.18.3.1.

--- gettext-tools/gnulib-lib/iconv_open-osf.h
+++ gettext-tools/gnulib-lib/iconv_open-osf.h
@@ -1,4 +1,4 @@
-/* ANSI-C code produced by gperf version 3.0.3 */
+/* ANSI-C code produced by gperf version 3.0.4 */
 /* Command-line: gperf -m 10 ./iconv_open-osf.gperf  */
 /* Computed positions: -k'4,$' */
 
@@ -251,7 +251,7 @@ static const struct mapping mappings[] =
 
 #ifdef __GNUC__
 __inline
-#ifdef __GNUC_STDC_INLINE__
+#if defined __GNUC_STDC_INLINE__ || defined __GNUC_GNU_INLINE__
 __attribute__ ((__gnu_inline__))
 #endif
 #endif
