97e425a74a2527d97d1f520bb17ed951e6990276
Václav Slavík <vaclav@slavik.io>
2014-07-04 22:11:53 +0200

    Increase version to 1.6.6, update NEWS.

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
-#define POEDIT_VERSION           "1.6.5"
-#define POEDIT_VERSION_WIN        1,6,5,0
+#define POEDIT_VERSION           "1.6.6"
+#define POEDIT_VERSION_WIN        1,6,6,0
 
 #endif // _VERSION_H_
