70dfff43a057109b25211e935ee7ff28fc97032e
Václav Slavík <vaclav@slavik.io>
2014-08-29 18:00:45 +0200

    Add a helper script for updating translations.
    Add a script that calls crowdin-cli and then fixes up some problems with
    it.  Currently, it removes empty .strings files and removes PO files
    where the only difference was in PO-Revision-Date.

---
+++ scripts/update-translations-from-crowdin.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+# Fetch translations updates:
+
+crowdin-cli download
+
+
+# Now work around deficiencies in Crowdin's CLI tools and remove bogus files:
+
+# Some PO files only have PO-Revision-Date difference:
+remove_unchanged_po_files()
+{
+    for i in locales/*.po ; do
+        if git diff --numstat "$i" | grep -q '^1\t1\t' ; then
+            if git diff "$i" | grep -q '^+"PO-Revision-Date:' ; then
+                git checkout "$i"
+            fi
+        fi
+    done
+}
+
+# Crowdin tools create empty .strings files for all translations, even the ones
+# that don't exist:
+remove_empty_lproj_string_files()
+{
+    find macosx -type f -name '*.strings' -empty -delete
+    find macosx -type d -name '*.lproj' -empty -delete
+}
+
+remove_unchanged_po_files
+remove_empty_lproj_string_files
+
+git status
