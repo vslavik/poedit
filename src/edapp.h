8efdc701adbd506a4a4a4a5a5b7dbbda863e7b76
Václav Slavík <vaclav@slavik.io>
2014-09-07 13:55:23 +0200

    Win: Associate with .po files w/o admin install
    If the installer was run without admin prileges, file association
    registry keys weren't created, because they are written into admin-only
    section of registry.  This allowed user-only installs, but not fully
    functional ones.
    
    Fix this by registering the association on first run if there is none.

--- src/edapp.h
+++ src/edapp.h
@@ -115,6 +115,7 @@ class PoeditApp : public wxApp
         void OnGettextManual(wxCommandEvent& event);
 
 #ifdef __WXMSW__
+        void AssociateFileTypeIfNeeded();
         void OnWinsparkleCheck(wxCommandEvent& event);
         static int WinSparkle_CanShutdown();
         static void WinSparkle_Shutdown();
