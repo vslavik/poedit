3e66add561eb806ce3d80a088e368845e8d160f6
Václav Slavík <vaclav@slavik.io>
2014-11-04 18:33:41 +0100

    Show comment icon in the list for autocomments too
    Don't show it only for translator-created comments, but also for notes
    for translators ("automatic comments").

--- src/edlistctrl.cpp
+++ src/edlistctrl.cpp
@@ -465,7 +465,7 @@ int PoeditListCtrl::OnGetItemImage(long item) const
 
     if (d.GetBookmark() != NO_BOOKMARK)
         return IMG_BOOKMARK;
-    else if (d.HasComment())
+    else if (d.HasComment() || d.HasAutoComments())
         return IMG_COMMENT;
     else if (d.IsAutomatic())
         return IMG_AUTOMATIC;
