vslavik/poedit

commit a05c3aa8f7a49948b79b22defa87c0df4c3bfa54

Fix out of date prefs on OS X when reopened
Because the preferences window is kept in existence once created and
data aren't transfered to it anew, the settings could get out of date
when showing the Preferences for the second (and more) time, if
something in the app changed them in the meantime.

Work around this by reloading them every time the prefs window is
activated or its tab changed. This still isn't perfect and may miss some
changes, but should be good enough for most cases.

src/prefsdlg.cpp
@@ -77,8 +77,24 @@ namespace
 class PrefsPanel : public wxPanel
 {
 public:
-    PrefsPanel(wxWindow *parent) : wxPanel(parent), m_inTransfer(false) {}
-    PrefsPanel() : wxPanel(), m_inTransfer(false) {}
+    PrefsPanel(wxWindow *parent)
+        : wxPanel(parent), m_inTransfer(false)
+    {
+#ifdef __WXOSX__
+        // Refresh the content of prefs panels when re-opening it.
+        // TODO: Use proper config settings notifications or user defaults bindings instead
+        parent->Bind(wxEVT_ACTIVATE, [=](wxActivateEvent& e){
+            e.Skip();
+            if (e.GetActive())
+                TransferDataToWindow();
+        });
+        Bind(wxEVT_SHOW, [=](wxShowEvent& e){
+            e.Skip();
+            if (e.IsShown())
+                TransferDataToWindow();
+        });
+#endif // __WXOSX__
+    }
 
     bool TransferDataToWindow() override
     {

commit aa0fdb0ce70443304ba790d2f2e2707619d04807

Fix duplicate extractors in preferences list
...on OS X.
Another thing broken by a05c3aa8.

src/prefsdlg.cpp
@@ -638,10 +638,11 @@ public:
     void InitValues(const wxConfigBase& cfg) override
     {
         m_extractors.Read(const_cast<wxConfigBase*>(&cfg));
-        
+
+        m_list->Clear();
         for (const auto& item: m_extractors.Data)
             m_list->Append(item.Name);
-        
+
         if (m_extractors.Data.empty())
         {
             m_edit->Enable(false);