vslavik/poedit

commit 187878d9af25b5777e7abc9d0e31baca777f1d7b

Sort results inside TranslationMemory code
Returned sorted results, from best to worst. Lucene normally returns
sorted results, but our internal score adjustments may change the order,
so the resorting is needed. Particularly relevant for callers that only
care about the best result.

src/tm/suggestions.h
@@ -26,6 +26,7 @@
 #ifndef Poedit_suggestions_h
 #define Poedit_suggestions_h
 
+#include <cmath>
 #include <functional>
 #include <memory>
 #include <string>
@@ -70,6 +71,14 @@ struct Suggestion
     bool HasScore() const { return score != 0.0; }
     bool IsExactMatch() const { return score == 1.0; }
 };
+
+inline bool operator<(const Suggestion& a, const Suggestion& b)
+{
+    if (std::fabs(a.score - b.score) <= std::numeric_limits<double>::epsilon())
+        return a.timestamp > b.timestamp;
+    else
+        return a.score > b.score;
+}
 
 typedef std::vector<Suggestion> SuggestionsList;
 

src/sidebar.cpp
@@ -474,13 +474,7 @@ void SuggestionsSidebarBlock::UpdateSuggestions(const SuggestionsList& hits)
     wxWindowUpdateLocker lock(m_parent);
 
     m_suggestions.insert(m_suggestions.end(), hits.begin(), hits.end());
-    std::stable_sort(m_suggestions.begin(), m_suggestions.end(),
-                     [](const Suggestion& a, const Suggestion& b){
-                        if (std::fabs(a.score - b.score) <= std::numeric_limits<double>::epsilon())
-                            return a.timestamp > b.timestamp;
-                        else
-                            return a.score > b.score;
-                     });
+    std::stable_sort(m_suggestions.begin(), m_suggestions.end());
 
     // create any necessary controls:
     while (m_suggestions.size() > m_suggestionsWidgets.size())

src/tm/transmem.cpp
@@ -242,6 +242,8 @@ void PerformSearch(IndexSearcherPtr searcher,
             }
         }
     );
+
+    std::stable_sort(results.begin(), results.end());
 }
 
 } // anonymous namespace
@@ -334,6 +336,7 @@ SuggestionsList TranslationMemoryImpl::Search(const Language& lang,
             }
         );
 
+        std::stable_sort(results.begin(), results.end());
         return results;
     }
     catch (LuceneException&)

commit 6f7fc7dd67b24f0b86d33f5b0c518d22150932bc

PCH-less compilation fix
Fixes compilation broken by 187878d9.

src/tm/suggestions.h
@@ -28,6 +28,7 @@
 
 #include <cmath>
 #include <functional>
+#include <limits>
 #include <memory>
 #include <string>
 #include <vector>