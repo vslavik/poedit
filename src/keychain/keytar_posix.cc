25e398118634d6275b063b05f9a3bc1928dbab91
Václav Slavík <vaclav@slavik.io>
2015-03-29 19:25:02 +0200

    Don't use io.github.atom.node-keytar libsecret schema
    Change the schema to net.poedit.Credentials.

--- src/keychain/keytar_posix.cc
+++ src/keychain/keytar_posix.cc
@@ -33,7 +33,7 @@ namespace keytar {
 namespace {
 
 const SecretSchema kSchema = {
-  "io.github.atom.node-keytar", SECRET_SCHEMA_NONE, {
+  "net.poedit.Credentials", SECRET_SCHEMA_NONE, {
     { "service", SECRET_SCHEMA_ATTRIBUTE_STRING },
     { "account", SECRET_SCHEMA_ATTRIBUTE_STRING },
     { NULL }
