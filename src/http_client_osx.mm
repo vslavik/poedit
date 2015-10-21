vslavik/poedit

commit 2c8dae7c1586bd69782cdf01745089dee8e04ba7

Use sensible User-Agent strings
On OS X, don't use AFNetworking batshit crazy string (e.g. Poedit/1.8.5
(Mac OS X Version 10.10.5 (Build 14F27)), localized!), but use a simple
token. On Windows, make the string a bit more detailed, by including OS
version string. This is the form the strings now have:

 * Poedit/1.8.5 (Mac OS X 10.11)
 * Poedit/1.8.5 (Windows NT 6.3)
 * Poedit/1.8.5 (Unix)

src/http_client_osx.mm
@@ -26,6 +26,7 @@
 #include "http_client.h"
 
 #include "str_helpers.h"
+#include "version.h"
 
 #import "AFHTTPClient.h"
 #import "AFJSONRequestOperation.h"
@@ -123,6 +124,12 @@ public:
 
         [m_native registerHTTPOperationClass:[AFJSONRequestOperation class]];
         [m_native setDefaultHeader:@"Accept" value:@"application/json"];
+
+        NSOperatingSystemVersion osx = [[NSProcessInfo processInfo] operatingSystemVersion];
+        NSString *osx_str = (osx.patchVersion == 0)
+                            ? [NSString stringWithFormat:@"%d.%d", (int)osx.majorVersion, (int)osx.minorVersion]
+                            : [NSString stringWithFormat:@"%d.%d.%d", (int)osx.majorVersion, (int)osx.minorVersion, (int)osx.patchVersion];
+        [m_native setDefaultHeader:@"User-Agent" value:[NSString stringWithFormat:@"Poedit/%s (Mac OS X %@)", POEDIT_VERSION, osx_str]];
     }
 
     ~impl()

src/http_client_casablanca.cpp
@@ -170,7 +170,7 @@ public:
         #define make_wide_str_(x) L ## x
 
         #if defined(_WIN32)
-            #define USER_AGENT_PLATFORM L" (Windows)"
+            #define USER_AGENT_PLATFORM L" (Windows NT " + windows_version() + L")"
         #elif defined(__unix__)
             #define USER_AGENT_PLATFORM L" (Unix)"
         #else
@@ -365,6 +365,15 @@ private:
         info.dwOSVersionInfoSize = sizeof(info);
         GetVersionEx(reinterpret_cast<OSVERSIONINFO*>(&info));
         return (info.dwMajorVersion < 6); // XP
+    }
+
+    static std::wstring windows_version()
+    {
+        OSVERSIONINFOEX info;
+        ZeroMemory(&info, sizeof(info));
+        info.dwOSVersionInfoSize = sizeof(info);
+        GetVersionEx(reinterpret_cast<OSVERSIONINFO*>(&info));
+        return std::to_wstring(info.dwMajorVersion) + L"." + std::to_wstring(info.dwMinorVersion);
     }
 
     INetworkListManager *m_networkListManager;

commit fb2d65f2be87f4c025ef20628f18a93a493fc464

Fix compatibility with pre-10.10 OS X
User-Agent change from 2c8dae7c broke Poedit on OS X older than
Yosemite, where it caused early abort, because [NSProcessInfo
operatingSystemVersion] wasn't available until 10.9.2 (and 10.10
officially).

Use the now-deprecated Gestalt() function on older systems.

src/http_client_osx.mm
@@ -130,10 +130,24 @@ public:
         // to the server.
         [m_native.operationQueue setMaxConcurrentOperationCount:NSIntegerMax];
 
-        NSOperatingSystemVersion osx = [[NSProcessInfo processInfo] operatingSystemVersion];
-        NSString *osx_str = (osx.patchVersion == 0)
-                            ? [NSString stringWithFormat:@"%d.%d", (int)osx.majorVersion, (int)osx.minorVersion]
-                            : [NSString stringWithFormat:@"%d.%d.%d", (int)osx.majorVersion, (int)osx.minorVersion, (int)osx.patchVersion];
+        int majorVersion, minorVersion, patchVersion;
+        NSProcessInfo *process = [NSProcessInfo processInfo];
+        if ([process respondsToSelector:@selector(operatingSystemVersion)])
+        {
+            NSOperatingSystemVersion osx = [process operatingSystemVersion];
+            majorVersion = (int)osx.majorVersion;
+            minorVersion = (int)osx.minorVersion;
+            patchVersion = (int)osx.patchVersion;
+        }
+        else
+        {
+            Gestalt(gestaltSystemVersionMajor, &majorVersion);
+            Gestalt(gestaltSystemVersionMinor, &minorVersion);
+            Gestalt(gestaltSystemVersionBugFix, &patchVersion);
+        }
+        NSString *osx_str = (patchVersion == 0)
+                            ? [NSString stringWithFormat:@"%d.%d", majorVersion, minorVersion]
+                            : [NSString stringWithFormat:@"%d.%d.%d", majorVersion, minorVersion, patchVersion];
         [m_native setDefaultHeader:@"User-Agent" value:[NSString stringWithFormat:@"Poedit/%s (Mac OS X %@)", POEDIT_VERSION, osx_str]];
     }
 