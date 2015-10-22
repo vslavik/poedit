747e3a7920e5c882c0ba24c06256639c874900f7
Václav Slavík <vaclav@slavik.io>
2015-10-22 17:58:48 +0200

    Use correct explanation text size even with wxGTK 3.0
    wxWINDOW_VARIANT_SMALL is too tiny in wxGTK. This was fixed on master,
    but not in 3.0, so make sure we use the correct size even there.

--- src/customcontrols.cpp
+++ src/customcontrols.cpp
@@ -201,8 +201,14 @@ SelectableAutoWrappingText::SelectableAutoWrappingText(wxWindow *parent, const w
 ExplanationLabel::ExplanationLabel(wxWindow *parent, const wxString& label)
     : AutoWrappingText(parent, label)
 {
-#if defined(__WXOSX__) || defined(__WXGTK__)
+#if defined(__WXOSX__)
     SetWindowVariant(wxWINDOW_VARIANT_SMALL);
+#elif defined(__WXGTK__)
+    #if wxCHECK_VERSION(3,1,0)
+        SetWindowVariant(wxWINDOW_VARIANT_SMALL);
+    #else
+        SetFont(GetFont().Smaller());
+    #endif
 #endif
 #ifndef __WXGTK__
     SetForegroundColour(GetTextColor());
@@ -223,8 +229,14 @@ wxColour ExplanationLabel::GetTextColor()
 SecondaryLabel::SecondaryLabel(wxWindow *parent, const wxString& label)
     : wxStaticText(parent, wxID_ANY, label)
 {
-#if defined(__WXOSX__) || defined(__WXGTK__)
+#if defined(__WXOSX__)
     SetWindowVariant(wxWINDOW_VARIANT_SMALL);
+#elif defined(__WXGTK__)
+    #if wxCHECK_VERSION(3,1,0)
+        SetWindowVariant(wxWINDOW_VARIANT_SMALL);
+    #else
+        SetFont(GetFont().Smaller());
+    #endif
 #endif
 #ifndef __WXGTK__
     SetForegroundColour(GetTextColor());
