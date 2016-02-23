vslavik/poedit

commit 0c39a4ab21c75568c0fb65fef1f13b7327d732ac

Preserve permissions when saving files on UNIX
Don't reset the permissions to their default values as a side effect to
creating a new temporary file and renaming it into the destination. Read
the original file's permissions and ownership, if it exists, and apply
them to the new file.

On OS X, use NSFileManager replaceItemAtURL: that does this (and more)
automatically.

src/utility.cpp
@@ -26,6 +26,7 @@
 #include "utility.h"
 
 #include <stdio.h>
+
 #include <wx/filename.h>
 #include <wx/log.h>
 #include <wx/config.h>
@@ -36,6 +37,11 @@
 
 #ifdef __WXOSX__
     #include <Foundation/Foundation.h>
+#endif
+#ifdef __UNIX__
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    #include <unistd.h>
 #endif
 
 #include "str_helpers.h"
@@ -168,7 +174,7 @@ wxString TempDirectory::CreateFileName(const wxString& suffix)
 
 
 // ----------------------------------------------------------------------
-// TempOutputFile
+// TempOutputFileFor
 // ----------------------------------------------------------------------
 
 TempOutputFileFor::TempOutputFileFor(const wxString& filename) : m_filenameFinal(filename)
@@ -225,7 +231,44 @@ TempOutputFileFor::TempOutputFileFor(const wxString& filename) : m_filenameFinal
 
 bool TempOutputFileFor::Commit()
 {
-    return wxRenameFile(m_filenameTmp, m_filenameFinal, /*overwrite=*/true);
+    return ReplaceFile(m_filenameTmp, m_filenameFinal);
+}
+
+bool TempOutputFileFor::ReplaceFile(const wxString& temp, const wxString& dest)
+{
+#ifdef __WXOSX__
+    NSURL *tempURL = [NSURL fileURLWithPath:str::to_NS(temp)];
+    NSURL *destURL = [NSURL fileURLWithPath:str::to_NS(dest)];
+    NSURL *resultingURL = nil;
+    return [[NSFileManager defaultManager] replaceItemAtURL:destURL
+                                              withItemAtURL:tempURL
+                                             backupItemName:nil
+                                                    options:0
+                                           resultingItemURL:&resultingURL
+                                                     error:nil];
+#else // !__WXOSX__
+  #ifdef __UNIX__
+    auto destPath = dest.fn_str();
+    bool overwrite = false;
+    struct stat st;
+
+    if ((overwrite = wxFileExists(dest)) == true)
+    {
+        if (stat(destPath, &st) != 0)
+            overwrite = false;
+    }
+  #endif
+
+    if (!wxRenameFile(temp, dest, /*overwrite=*/true))
+        return false;
+
+  #ifdef __UNIX__
+    chown(destPath, st.st_uid, st.st_gid);
+    chmod(destPath, st.st_mode);
+  #endif
+
+    return true;
+#endif // !__WXOSX__
 }
 
 TempOutputFileFor::~TempOutputFileFor()

src/catalog.cpp
@@ -1583,7 +1583,7 @@ bool Catalog::Save(const wxString& po_file, bool save_mo,
                 finalFile.Write(outputCrlf);
         }
 
-        if (!wxRenameFile(po_file_temp2, po_file, /*overwrite=*/true))
+        if (!TempOutputFileFor::ReplaceFile(po_file_temp2, po_file))
             msgcat_ok = false;
     }
 
@@ -1593,7 +1593,7 @@ bool Catalog::Save(const wxString& po_file, bool save_mo,
     }
     else
     {
-        if ( !wxRenameFile(po_file_temp, po_file, /*overwrite=*/true) )
+        if ( !po_file_temp_obj.Commit() )
         {
             wxLogError(_("Couldn't save file %s."), po_file.c_str());
         }
@@ -1685,7 +1685,7 @@ bool Catalog::Save(const wxString& po_file, bool save_mo,
                 [NSFileCoordinator removeFilePresenter:presenter];
             }
 #else // !__WXOSX__
-            if ( !wxRenameFile(mo_file_temp, mo_file, /*overwrite=*/true) )
+            if ( !mo_file_temp_obj.Commit() )
             {
                 wxLogError(_("Couldn't save file %s."), mo_file.c_str());
                 mo_compilation_status = CompilationStatus::Error;
@@ -1774,7 +1774,7 @@ bool Catalog::CompileToMO(const wxString& mo_file,
         mo_compilation_status = CompilationStatus::Success;
     }
 
-    if ( !wxRenameFile(mo_file_temp, mo_file, /*overwrite=*/true) )
+    if ( !mo_file_temp_obj.Commit() )
     {
         wxLogError(_("Couldn't save file %s."), mo_file.c_str());
         return false;

src/utility.h
@@ -214,6 +214,12 @@ public:
     /// Renames temp file to the final one (passed to ctor).
     bool Commit();
 
+    /// Rename file to replace another *while preserving destination
+    /// file's permissions*.
+    /// Make this helper publicly accessible for code that can't
+    /// use TempOutputFileFor directly.
+    static bool ReplaceFile(const wxString& temp, const wxString& dest);
+
 #ifdef __WXOSX__
     wxString m_tempDir;
 #endif

commit f4a0cd267d035d17a4b118578e8a086af6b42ba0

Fix setting of new files' permissions on Unix
The changes in 0c39a4ab21c75568c0fb65fef1f13b7327d732ac accidentally
tried to preserve permissions of existing file even if one didn't exist,
leading to setting essentially random permissions.

src/utility.cpp
@@ -263,8 +263,11 @@ bool TempOutputFileFor::ReplaceFile(const wxString& temp, const wxString& dest)
         return false;
 
   #ifdef __UNIX__
-    chown(destPath, st.st_uid, st.st_gid);
-    chmod(destPath, st.st_mode);
+    if (overwrite)
+    {
+        chown(destPath, st.st_uid, st.st_gid);
+        chmod(destPath, st.st_mode);
+    }
   #endif
 
     return true;