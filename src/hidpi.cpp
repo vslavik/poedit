vslavik/poedit

commit 88de2889150f4f1250a97499467024a60815876b

Improve toolbar buttons look on Windows 10
There's barely any padding on Windows 10 in the toolbar, so add it
artificially: enlarge toolbar icons by 2px.

src/hidpi.cpp
@@ -42,6 +42,7 @@ void InitHiDPIHandling()
 
 namespace
 {
+
 void LoadPNGImage(wxImage& img, const wxString& filename)
 {
     img.LoadFile(filename, wxBITMAP_TYPE_PNG);
@@ -54,28 +55,22 @@ void LoadPNGImage(wxImage& img, const wxString& filename)
     }
 }
 
-} // anonymous namespace
-
-#endif // NEEDS_MANUAL_HIDPI
-
-
-wxBitmap LoadScaledBitmap(const wxString& name, bool mirror)
+bool LoadAndScale(wxImage& img, const wxString& name)
 {
     const wxString filename(name + ".png");
     if (!wxFileExists(filename))
-        return wxNullBitmap;
+        return false;
 
 #ifdef NEEDS_MANUAL_HIDPI
     if (HiDPIScalingFactor() > 1.0)
     {
-        wxImage img;
         double imgScale = HiDPIScalingFactor();
         const wxString filename_2x(name + "@2x.png");
         if (wxFileExists(filename_2x))
         {
             LoadPNGImage(img, filename_2x);
             if (HiDPIScalingFactor() == 2.0)
-                return wxBitmap(img);
+                return true;
             else
                 imgScale /= 2.0;
         }
@@ -92,12 +87,34 @@ wxBitmap LoadScaledBitmap(const wxString& name, bool mirror)
         else
             quality = wxIMAGE_QUALITY_BICUBIC;
         img.Rescale(img.GetWidth() * imgScale, img.GetHeight() * imgScale, quality);
-        return wxBitmap(img);
+        return true;
     }
     // else: load normally
 #endif
 
-    auto img = wxImage(filename, wxBITMAP_TYPE_PNG);
+    img = wxImage(filename, wxBITMAP_TYPE_PNG);
+    return true;
+}
+
+} // anonymous namespace
+
+#endif // NEEDS_MANUAL_HIDPI
+
+
+wxBitmap LoadScaledBitmap(const wxString& name, bool mirror, int padding)
+{
+    wxImage img;
+    if (!LoadAndScale(img, name))
+        return wxNullBitmap;
+
+    if (padding)
+    {
+        int pad = PX(padding);
+        auto sz = img.GetSize();
+        sz.IncBy(pad * 2);
+        img.Resize(sz, wxPoint(pad, pad));
+    }
+
     if (mirror)
         return wxBitmap(img.Mirror());
     else

src/hidpi.h
@@ -80,6 +80,6 @@ inline double HiDPIScalingFactor() { return 1.0; }
 
     Note that @a name is given *without* the ".png" extension.
  */
-extern wxBitmap LoadScaledBitmap(const wxString& name, bool mirror);
+extern wxBitmap LoadScaledBitmap(const wxString& name, bool mirror, int padding = 0);
 
 #endif // Poedit_hidpi_h

src/icons.cpp
@@ -32,6 +32,7 @@
 #include "icons.h"
 #include "edapp.h"
 #include "hidpi.h"
+#include "utility.h"
 
 #ifndef __WXOSX__
 
@@ -128,10 +129,16 @@ wxBitmap PoeditArtProvider::CreateBitmap(const wxArtID& id,
     if (wxTheApp->GetLayoutDirection() == wxLayout_RightToLeft)
         mirror = ShouldBeMirorredInRTL(id, client);
 
+    int padding = 0;
+#ifdef __WXMSW__
+    if (IsWindows10OrGreater())
+        padding = 2;
+#endif
+
     wxString icon;
     icon.Printf("%s/%s", iconsdir.c_str(), id.c_str());
     wxLogTrace("poedit.icons", "loading from %s", icon.c_str());
-    return LoadScaledBitmap(icon, mirror);
+    return LoadScaledBitmap(icon, mirror, padding);
 }
 
 #endif // !__WXOSX__

commit a25d9ca2bfcf0b8e18eef95fadcb4f56e3fef150

Fix OS X compilation
Broken by 88de2889.

src/hidpi.cpp
@@ -40,6 +40,8 @@ void InitHiDPIHandling()
     g_pxScalingFactor = dpi.y / 96.0;
 }
 
+#endif // NEEDS_MANUAL_HIDPI
+
 namespace
 {
 
@@ -92,13 +94,11 @@ bool LoadAndScale(wxImage& img, const wxString& name)
     // else: load normally
 #endif
 
-    img = wxImage(filename, wxBITMAP_TYPE_PNG);
+    LoadPNGImage(img, filename);
     return true;
 }
 
 } // anonymous namespace
-
-#endif // NEEDS_MANUAL_HIDPI
 
 
 wxBitmap LoadScaledBitmap(const wxString& name, bool mirror, int padding)