819fede6fbc0c0c1a7e379c70aa37deabd728012
Václav Slavík <vaclav@slavik.io>
2017-01-01 18:40:40 +0100

    Import more wx strings
    See #310.
    
    Fixes #309.

--- src/wx_translatable_strings.h
+++ src/wx_translatable_strings.h
@@ -49,7 +49,7 @@ _("%s Preferences");
 
 
 // --------------------------------------------------------------------------
-// macOS menus
+// macOS
 // --------------------------------------------------------------------------
 
 // application menu:
@@ -69,16 +69,49 @@ _("Preferences...");
 
 
 // --------------------------------------------------------------------------
+// Windows
+// --------------------------------------------------------------------------
+
+// system taskbar categories:
+
+// TRANSLATORS: Title of a category in Windows task menu (right-click icon in taskbar)
+_("Recent");
+// TRANSLATORS: Title of a category in Windows task menu (right-click icon in taskbar)
+_("Frequent");
+
+
+// --------------------------------------------------------------------------
 // Stock menu/button iems
 // --------------------------------------------------------------------------
 
-_("&Undo"),               _("Undo");
-_("&Redo"),               _("Redo");
-_("Cu&t"),                _("Cut");
-_("&Copy"),               _("Copy");
-_("&Paste"),              _("Paste");
-_("&Delete"),             _("Delete");
-_("Select &All"),         _("Select All");
+STOCKITEM(wxID_APPLY,               _("&Apply"),              _("Apply"));
+STOCKITEM(wxID_BACKWARD,            _("&Back"),               _("Back"));
+STOCKITEM(wxID_CANCEL,              _("&Cancel"),             _("Cancel"));
+STOCKITEM(wxID_CLEAR,               _("&Clear"),              _("Clear"));
+STOCKITEM(wxID_CLOSE,               _("&Close"),              _("Close"));
+
+STOCKITEM(wxID_COPY,                _("&Copy"),               _("Copy"));
+STOCKITEM(wxID_CUT,                 _("Cu&t"),                _("Cut"));
+STOCKITEM(wxID_DELETE,              _("&Delete"),             _("Delete"));
+
+STOCKITEM(wxID_EDIT,                _("&Edit"),               _("Edit"));
+STOCKITEM(wxID_EXIT,                _("&Quit"),               _("Quit"));
+STOCKITEM(wxID_FILE,                _("&File"),               _("File"));
+STOCKITEM(wxID_HELP,                _("&Help"),               _("Help"));
+STOCKITEM(wxID_NEW,                 _("&New"),                _("New"));
+STOCKITEM(wxID_NO,                  _("&No"),                 _("No"));
+STOCKITEM(wxID_OK,                  _("&OK"),                 _("OK"));
+STOCKITEM(wxID_OPEN,                _("&Open..."),            _("Open..."));
+STOCKITEM(wxID_PASTE,               _("&Paste"),              _("Paste"));
+STOCKITEM(wxID_PREFERENCES,         _("&Preferences"),        _("Preferences"));
+STOCKITEM(wxID_REDO,                _("&Redo"),               _("Redo"));
+STOCKITEM(wxID_REFRESH,             _("Refresh"),             _("Refresh"));
+STOCKITEM(wxID_SAVE,                _("&Save"),               _("Save"));
+STOCKITEM(wxID_SAVEAS,              _("&Save as"),            _("Save as"));
+STOCKITEM(wxID_SELECTALL,           _("Select &All"),         _("Select All"));
+STOCKITEM(wxID_UNDO,                _("&Undo"),               _("Undo"));
+STOCKITEM(wxID_YES,                 _("&Yes"),                _("Yes"));
+
 
 /// TRANSLATORS: Keyboard shortcut for display in Windows menus
 _("Ctrl+");
