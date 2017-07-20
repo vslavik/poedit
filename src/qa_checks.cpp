e93630c80eb39b55692d40c08edef37a32efe7b3
Václav Slavík <vaclav@slavik.io>
2017-07-20 15:44:31 +0200

    Add full-width ( and ) to zh/ja QA rules

--- src/qa_checks.cpp
+++ src/qa_checks.cpp
@@ -217,6 +217,10 @@ private:
                     return trans == L'？';
                 case ':':
                     return trans == L'：';
+                case '(':
+                    return trans == L'（';
+                case ')':
+                    return trans == L'）';
                 default:
                     break;
             }
