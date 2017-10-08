vslavik/poedit

commit ba12bb09e7be478e326f20bcb58026e85ae68e16

Fix plural forms UI sizing with custom font
If custom text fields font is set, editing area can sometimes get
disproportionally larger in the upper part, sometimes not leaving
enough place for actual edit controls.

Fix by explicitly resetting minimal height.

Fixes #348.

src/editing_area.cpp
@@ -526,6 +526,14 @@ void EditingArea::SetCustomFont(const wxFont& font)
     SetCtrlFont(m_textTrans, font);
     for (auto tp : m_textTransPlural)
         SetCtrlFont(tp, font);
+
+    int minh = m_textOrig->GetCharHeight();
+#ifdef __WXOSX__
+    minh += 2*3; // inset
+#endif
+
+    m_textOrig->SetMinSize(wxSize(-1, minh));
+    m_textOrigPlural->SetMinSize(wxSize(-1, minh));
 }
 
 

commit 10f2836c5bb352dd7569bb57961c1e6799d7bd0d

Fix rendering regression caused by ba12bb0

src/editing_area.cpp
@@ -252,7 +252,13 @@ private:
 
         auto rect = GetClientRect();
         if (!rect.IsEmpty())
+        {
+#if wxCHECK_VERSION(3,1,1)
+            gc->DrawRoundedRectangle(rect.x, rect.y, rect.width, rect.height, PX(2));
+#else
             gc->DrawRectangle(rect.x, rect.y, rect.width, rect.height);
+#endif
+        }
     }
 
     wxColour m_fg, m_bg;