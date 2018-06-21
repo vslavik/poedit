vslavik/poedit

commit 229ffa47191cc17e215f02575dfc65222cf191ab

Check plural forms correctness more robustly
Instead of comparing the expressions as strings and showing a warning
on any difference, evaluate them for true equivalence.

src/language.cpp
@@ -31,6 +31,8 @@
 #include <mutex>
 #include <memory>
 
+#include <boost/algorithm/string.hpp>
+
 #include <unicode/uvernum.h>
 #include <unicode/locid.h>
 #include <unicode/coll.h>
@@ -39,6 +41,7 @@
 #include <wx/filename.h>
 
 #include "str_helpers.h"
+#include "pluralforms/pl_evaluate.h"
 
 #ifdef HAVE_CLD2
     #ifdef HAVE_CLD2_PUBLIC_COMPACT_LANG_DET_H
@@ -386,10 +389,10 @@ Language Language::FromLegacyNames(const std::string& lang, const std::string& c
 }
 
 
-std::string Language::DefaultPluralFormsExpr() const
+PluralFormsExpr Language::DefaultPluralFormsExpr() const
 {
     if (!IsValid())
-        return std::string();
+        return PluralFormsExpr();
 
     static const std::unordered_map<std::string, std::string> forms = {
         #include "language_impl_plurals.h"
@@ -407,7 +410,7 @@ std::string Language::DefaultPluralFormsExpr() const
     if ( i != forms.end() )
         return i->second;
 
-    return std::string();
+    return PluralFormsExpr();
 }
 
 
@@ -561,4 +564,62 @@ Language Language::TryDetectFromText(const char *buffer, size_t len, Language pr
     (void)len;
     return probableLanguage;
 #endif
+}
+
+
+PluralFormsExpr::PluralFormsExpr() : m_calcCreated(true)
+{
+}
+
+PluralFormsExpr::PluralFormsExpr(const std::string& expr) : m_expr(expr), m_calcCreated(false)
+{
+}
+
+PluralFormsExpr::~PluralFormsExpr()
+{
+}
+
+std::shared_ptr<PluralFormsCalculator> PluralFormsExpr::calc() const
+{
+    auto self = const_cast<PluralFormsExpr*>(this);
+    if (m_calcCreated)
+        return m_calc;
+    if (!m_expr.empty())
+        self->m_calc = PluralFormsCalculator::make(m_expr.c_str());
+    self->m_calcCreated = true;
+    return m_calc;
+}
+
+bool PluralFormsExpr::operator==(const PluralFormsExpr& other) const
+{
+    if (m_expr == other.m_expr)
+        return true;
+
+    // do some normalization to avoid unnecessary complains when the only
+    // differences are in whitespace for example:
+    auto expr1 = boost::erase_all_copy(m_expr, " \t");
+    auto expr2 = boost::erase_all_copy(other.m_expr, " \t");
+    if (expr1 == expr2)
+        return true;
+
+    // failing that, compare the expressions semantically:
+    auto calc1 = calc();
+    auto calc2 = other.calc();
+
+    if (calc1->nplurals() != calc2->nplurals())
+        return false;
+
+    for (int i = 0; i < MAX_EXAMPLES_COUNT; i++)
+    {
+        if (calc1->evaluate(i) != calc2->evaluate(i))
+            return false;
+    }
+    // both expressions are identical on all tested integers
+    return true;
+}
+
+int PluralFormsExpr::evaluate_for_n(int n) const
+{
+    auto c = calc();
+    return c ? c->evaluate(n) : 0;
 }

Poedit.xcodeproj/project.pbxproj
@@ -15,6 +15,8 @@
 		B209006019CAD64A00D6382E /* SuggestionError.png in Resources */ = {isa = PBXBuildFile; fileRef = B209005F19CAD64A00D6382E /* SuggestionError.png */; };
 		B20960F319928C8500A2EB13 /* GettextToolsDummy.c in Sources */ = {isa = PBXBuildFile; fileRef = B20960F219928C8500A2EB13 /* GettextToolsDummy.c */; };
 		B20F24FB1E39113900906CA8 /* extractor_gettext.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B20F24FA1E39113900906CA8 /* extractor_gettext.cpp */; };
+		B212FEED20A7356300FAC68F /* pl_evaluate.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CE316F629D30018AF7E /* pl_evaluate.cpp */; };
+		B212FEEE20A7366600FAC68F /* pl_evaluate.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CE316F629D30018AF7E /* pl_evaluate.cpp */; };
 		B2132FDA19B3672000326B16 /* customcontrols.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B2132FD819B3672000326B16 /* customcontrols.cpp */; };
 		B2166F4919AE4045000A6AA0 /* Prefs-Extractors.png in Resources */ = {isa = PBXBuildFile; fileRef = B2166F4119AE4045000A6AA0 /* Prefs-Extractors.png */; };
 		B2166F4A19AE4045000A6AA0 /* Prefs-Extractors@2x.png in Resources */ = {isa = PBXBuildFile; fileRef = B2166F4219AE4045000A6AA0 /* Prefs-Extractors@2x.png */; };
@@ -89,7 +91,6 @@
 		B28F1CFC16F629D30018AF7E /* transmem.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CD816F629D30018AF7E /* transmem.cpp */; };
 		B28F1CFF16F629D30018AF7E /* utility.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CDE16F629D30018AF7E /* utility.cpp */; };
 		B28F1D0016F629D30018AF7E /* export_html.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CE216F629D30018AF7E /* export_html.cpp */; };
-		B28F1D0116F629D30018AF7E /* pl_evaluate.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B28F1CE316F629D30018AF7E /* pl_evaluate.cpp */; };
 		B295C6021E2A81C200CD71CD /* extractor_legacy.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B295C5FE1E2A81C200CD71CD /* extractor_legacy.cpp */; };
 		B295C6031E2A81C200CD71CD /* extractor.cpp in Sources */ = {isa = PBXBuildFile; fileRef = B295C6001E2A81C200CD71CD /* extractor.cpp */; };
 		B29A1C331A9F8C0A00BC3006 /* poedit-sync.png in Resources */ = {isa = PBXBuildFile; fileRef = B29A1C311A9F8C0A00BC3006 /* poedit-sync.png */; };
@@ -1382,6 +1383,7 @@
 				B28F1CF216F629D30018AF7E /* gexecute.cpp in Sources */,
 				B2A3637C1E4B9DC800E96253 /* pretranslate.cpp in Sources */,
 				B28F1CF516F629D30018AF7E /* manager.cpp in Sources */,
+				B212FEED20A7356300FAC68F /* pl_evaluate.cpp in Sources */,
 				B28F1CF616F629D30018AF7E /* macos_helpers.mm in Sources */,
 				B240FFC719C6F1A600777AFE /* suggestions.cpp in Sources */,
 				B2380F9A1A9B821200B7D8C9 /* crowdin_gui.cpp in Sources */,
@@ -1397,7 +1399,6 @@
 				B28F1D0016F629D30018AF7E /* export_html.cpp in Sources */,
 				B230E2281A73F81400FB1E57 /* hidpi.cpp in Sources */,
 				B280E84D1A92776D009F4A98 /* http_client_macos.mm in Sources */,
-				B28F1D0116F629D30018AF7E /* pl_evaluate.cpp in Sources */,
 				B22C5F0A17DDC67400ECAFD1 /* language.cpp in Sources */,
 				B2E2184C199A76B100EA2784 /* syntaxhighlighter.cpp in Sources */,
 				B2284A53183BE3B300E097C7 /* PFMoveApplication.m in Sources */,
@@ -1419,6 +1420,7 @@
 				B2DAD70F1AD1984200DCB398 /* utility.cpp in Sources */,
 				B2DAD7101AD198B800DCB398 /* gexecute.cpp in Sources */,
 				B2CE6D211ACFCD95007E6863 /* GeneratePreviewForURL.cpp in Sources */,
+				B212FEEE20A7366600FAC68F /* pl_evaluate.cpp in Sources */,
 				B2CE6D231ACFCD95007E6863 /* main.c in Sources */,
 			);
 			runOnlyForDeploymentPostprocessing = 0;

src/catalog.cpp
@@ -1378,7 +1378,7 @@ void Catalog::FixupCommonIssues()
         // Auto-fill default plural form if it is missing:
         if (m_header.Lang.IsValid() && HasPluralItems())
         {
-            pluralForms = m_header.Lang.DefaultPluralFormsExpr();
+            pluralForms = m_header.Lang.DefaultPluralFormsExpr().str();
             if (!pluralForms.empty())
                 m_header.SetHeader("Plural-Forms", pluralForms);
         }
@@ -2426,7 +2426,7 @@ bool Catalog::HasPluralItems() const
 void Catalog::SetLanguage(Language lang)
 {
     m_header.Lang = lang;
-    m_header.SetHeaderNotEmpty("Plural-Forms", lang.DefaultPluralFormsExpr());
+    m_header.SetHeaderNotEmpty("Plural-Forms", lang.DefaultPluralFormsExpr().str());
 }
 
 void Catalog::GetStatistics(int *all, int *fuzzy, int *badtokens,

src/edframe.cpp
@@ -77,7 +77,6 @@
 #include "commentdlg.h"
 #include "main_toolbar.h"
 #include "manager.h"
-#include "pluralforms/pl_evaluate.h"
 #include "pretranslate.h"
 #include "attentionbar.h"
 #include "utility.h"
@@ -2267,19 +2266,16 @@ void PoeditFrame::WarnAboutLanguageIssues()
         }
 
         // FIXME: make this part of global error checking
-        wxString plForms = m_catalog->Header().GetHeader("Plural-Forms");
-        auto plCalc = PluralFormsCalculator::make(plForms.ToAscii());
-        if ( !plCalc )
+        PluralFormsExpr plForms(m_catalog->Header().GetHeader("Plural-Forms").ToStdString());
+        if (!plForms)
         {
-            if ( plForms.empty() )
+            if (plForms.str().empty())
             {
                 err = _("Required header Plural-Forms is missing.");
             }
             else
             {
-                err = wxString::Format(
-                            _("Syntax error in Plural-Forms header (\"%s\")."),
-                            plForms.c_str());
+                err = wxString::Format(_("Syntax error in Plural-Forms header (\"%s\")."), plForms.str());
             }
         }
 
@@ -2298,48 +2294,26 @@ void PoeditFrame::WarnAboutLanguageIssues()
         }
         else // no error, check for warning-worthy stuff
         {
-            if ( lang.IsValid() )
+            if (lang.IsValid() && plForms != lang.DefaultPluralFormsExpr())
             {
-                // Check for unusual plural forms. Do some normalization to avoid unnecessary
-                // complains when the only differences are in whitespace for example.
-                wxString pl1 = plForms;
-                wxString pl2 = lang.DefaultPluralFormsExpr();
-                if (!pl2.empty())
-                {
-                    pl1.Replace(" ", "");
-                    pl2.Replace(" ", "");
-                    if ( pl1 != pl2 )
-                    {
-                        if (pl1.Find(";plural=(") == wxNOT_FOUND && pl1.Last() == ';')
-                        {
-                            pl1.Replace(";plural=", ";plural=(");
-                            pl1.RemoveLast();
-                            pl1 += ");";
-                        }
-                    }
+                AttentionMessage msg
+                    (
+                        "unusual-plural-forms",
+                        AttentionMessage::Warning,
+                        wxString::Format
+                        (
+                            // TRANSLATORS: %s is language name in its basic form (as you
+                            // would see e.g. in a list of supported languages). You may need
+                            // to rephrase it, e.g. to an equivalent of "for language %s".
+                            _("Plural forms expression used by the catalog is unusual for %s."),
+                            lang.DisplayName()
+                        )
+                    );
+                // TRANSLATORS: A verb, shown as action button with ""Plural forms expression used by the catalog is unusual for %s.")"
+                msg.AddAction(_("Review"), [=]{ EditCatalogProperties(); });
+                msg.AddDontShowAgain();
 
-                    if ( pl1 != pl2 )
-                    {
-                        AttentionMessage msg
-                            (
-                                "unusual-plural-forms",
-                                AttentionMessage::Warning,
-                                wxString::Format
-                                (
-                                    // TRANSLATORS: %s is language name in its basic form (as you
-                                    // would see e.g. in a list of supported languages). You may need
-                                    // to rephrase it, e.g. to an equivalent of "for language %s".
-                                    _("Plural forms expression used by the catalog is unusual for %s."),
-                                    lang.DisplayName()
-                                )
-                            );
-                        // TRANSLATORS: A verb, shown as action button with ""Plural forms expression used by the catalog is unusual for %s.")"
-                        msg.AddAction(_("Review"), [=]{ EditCatalogProperties(); });
-                        msg.AddDontShowAgain();
-
-                        m_attentionBar->ShowMessage(msg);
-                    }
-                }
+                m_attentionBar->ShowMessage(msg);
             }
         }
     }

src/editing_area.cpp
@@ -30,7 +30,6 @@
 #include "customcontrols.h"
 #include "edlistctrl.h"
 #include "hidpi.h"
-#include "pluralforms/pl_evaluate.h"
 #include "spellchecking.h"
 #include "text_control.h"
 #include "utility.h"
@@ -624,7 +623,7 @@ void EditingArea::RecreatePluralTextCtrls(CatalogPtr catalog)
     m_pluralNotebook->DeleteAllPages();
     m_textTransSingularForm = NULL;
 
-    auto calc = PluralFormsCalculator::make(catalog->Header().GetHeader("Plural-Forms").ToAscii());
+    auto plurals = PluralFormsExpr(catalog->Header().GetHeader("Plural-Forms").ToStdString());
 
     int formsCount = catalog->GetPluralFormsCount();
     for (int form = 0; form < formsCount; form++)
@@ -635,11 +634,11 @@ void EditingArea::RecreatePluralTextCtrls(CatalogPtr catalog)
         int firstExample = -1;
         int examplesCnt = 0;
 
-        if (calc && formsCount > 1)
+        if (plurals && formsCount > 1)
         {
-            for (int example = 0; example < 1000; example++)
+            for (int example = 0; example < PluralFormsExpr::MAX_EXAMPLES_COUNT; example++)
             {
-                if (calc->evaluate(example) == form)
+                if (plurals.evaluate_for_n(example) == form)
                 {
                     if (++examplesCnt == 1)
                         firstExample = example;

src/language.h
@@ -27,9 +27,14 @@
 #define Poedit_language_h
 
 #include <wx/string.h>
+#include <memory>
 #include <string>
 #include <vector>
 #include <unicode/locid.h>
+
+class PluralFormsCalculator;
+class PluralFormsExpr;
+
 
 /// Language's text writing direction
 enum class TextDirection
@@ -96,7 +101,7 @@ public:
         @return Plural form expression suitable for directly using in the
                 gettext header or empty string if no record was found.
      */
-    std::string DefaultPluralFormsExpr() const;
+    PluralFormsExpr DefaultPluralFormsExpr() const;
 
     /// Returns language's text writing direction
     TextDirection Direction() const { return m_direction; }
@@ -173,6 +178,33 @@ private:
     std::string m_code;
     std::string m_tag;
     TextDirection m_direction;
+};
+
+
+/// Language's plural forms expression
+class PluralFormsExpr
+{
+public:
+    /// What numbers to test or show examples for (0..1001)
+    static const int MAX_EXAMPLES_COUNT = 1002;
+
+    PluralFormsExpr();
+    PluralFormsExpr(const std::string& expr);
+    ~PluralFormsExpr();
+
+    const std::string& str() const { return m_expr; }
+    bool operator==(const PluralFormsExpr& other) const;
+    bool operator!=(const PluralFormsExpr& other) const { return !(*this == other); }
+    explicit operator bool() const { return !m_expr.empty() && calc() != nullptr; }
+
+    int evaluate_for_n(int n) const;
+
+private:
+    std::shared_ptr<PluralFormsCalculator> calc() const;
+
+    std::string m_expr;
+    bool m_calcCreated;
+    std::shared_ptr<PluralFormsCalculator> m_calc;
 };
 
 #endif // Poedit_language_h

src/pluralforms/pl_evaluate.cpp
@@ -577,9 +577,9 @@ PluralFormsNode* PluralFormsParser::pmExpression()
     return n.release();
 }
 
-std::unique_ptr<PluralFormsCalculator> PluralFormsCalculator::make(const char* s)
+std::shared_ptr<PluralFormsCalculator> PluralFormsCalculator::make(const char* s)
 {
-    auto calculator = std::make_unique<PluralFormsCalculator>();
+    auto calculator = std::make_shared<PluralFormsCalculator>();
     if (s != NULL)
     {
         PluralFormsScanner scanner(s);

src/pluralforms/pl_evaluate.h
@@ -133,10 +133,12 @@ public:
     // input: number, returns msgstr index
     int evaluate(int n) const;
 
+    int nplurals() const { return m_nplurals; }
+
     // input: text after "Plural-Forms:" (e.g. "nplurals=2; plural=(n != 1);"),
     // if s == 0, creates default handler
     // returns 0 if error
-    static std::unique_ptr<PluralFormsCalculator> make(const char* s = 0);
+    static std::shared_ptr<PluralFormsCalculator> make(const char* s = 0);
 
     ~PluralFormsCalculator() {}
 

commit 958395b941e40da8769fa16d39f2f05c76df12bc

Fix PluralFormsExpr::operator==() crash
Fix crash when comparing two different expressions, at least one of
them invalid. The bug was introduced in 229ffa47.

src/language.cpp
@@ -606,6 +606,9 @@ bool PluralFormsExpr::operator==(const PluralFormsExpr& other) const
     auto calc1 = calc();
     auto calc2 = other.calc();
 
+    if (!calc1 || !calc2)
+        return false; // at least one is invalid _and_ the strings are different due to code above
+
     if (calc1->nplurals() != calc2->nplurals())
         return false;
 