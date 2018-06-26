7f167db47629c9df5ffaf39ada9c37a9c50c321b
Václav Slavík <vaclav@slavik.io>
2018-06-26 13:40:01 +0200

    Show suggestions actions button only when needed
    18bc4c50 was broken and unconditionally enabled the … button instead of
    using ShouldShowActions() as it was supposed to. Fixed now.

--- src/sidebar.cpp
+++ src/sidebar.cpp
@@ -451,7 +451,7 @@ private:
         m_isHighlighted = highlight;
         for (auto c: GetChildren())
             c->SetBackgroundColour(highlight ? m_bgHighlight : m_bg);
-        m_moreActions->Show(highlight);
+        m_moreActions->Show(highlight && ShouldShowActions());
         Refresh();
 
         if (highlight)
