29edfc8e4632a3514d49c5bf0a21ac6c8a8f6e28
Václav Slavík <vaclav@slavik.io>
2018-10-07 12:38:29 +0200

    Increase version to 2.2, update NEWS

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef Poedit_version_h
 #define Poedit_version_h
 
-#define POEDIT_VERSION           "2.1.1"
-#define POEDIT_VERSION_WIN        2,1,1
+#define POEDIT_VERSION           "2.2"
+#define POEDIT_VERSION_WIN        2,2,0
 
 #endif // Poedit_version_h
