vslavik/poedit

commit b96300abe91c6b3adef6a4ddddd818e105e49896

Simplify beta versions handling
Don’t put this indicator into version number (POEDIT_VERSION) - it was
mostly used incorrectly in the code. Instead, hardcode it in the
IsBetaVersion() function, and remove all the superfluous and confusing
version macros.

win32/poedit.iss
@@ -31,7 +31,6 @@
 #include "../" + CONFIG + "/git_build_number.h"
 
 #define VERSION          "2.1"
-#define VERSION_FULL     "2.1"
 #define VERSION_WIN      VERSION + "." + Str(POEDIT_GIT_BUILD_NUMBER)
 
 #ifndef CRT_REDIST
@@ -46,7 +45,7 @@ OutputBaseFilename=Poedit-{#VERSION_FULL}-setup
 OutputDir=win32\distrib-{#CONFIG}-{#VERSION_FULL}
 
 AppName=Poedit
-AppVerName=Poedit {#VERSION_FULL}
+AppVerName=Poedit {#VERSION}
 
 PrivilegesRequired=none
 ChangesAssociations=true
@@ -69,12 +68,12 @@ AllowUNCPath=true
 InternalCompressLevel=ultra
 AppID={{68EB2C37-083A-4303-B5D8-41FA67E50B8F}
 VersionInfoVersion={#VERSION_WIN}
-VersionInfoTextVersion={#VERSION_FULL}
+VersionInfoTextVersion={#VERSION}
 AppCopyright=Copyright © 1999-2018 Vaclav Slavik
 AppPublisher=Vaclav Slavik
 AppSupportURL=https://poedit.net/support
 AppUpdatesURL=https://poedit.net/download
-AppVersion={#VERSION_FULL}
+AppVersion={#VERSION}
 AppContact=help@poedit.net
 UninstallDisplayIcon={app}\Poedit.exe
 UninstallDisplayName=Poedit
@@ -91,7 +90,7 @@ VersionInfoDescription=Poedit Installer
 VersionInfoCopyright=Copyright © 1999-2018 Vaclav Slavik
 VersionInfoProductName=Poedit
 VersionInfoProductVersion={#VERSION_WIN}
-VersionInfoProductTextVersion={#VERSION_FULL}
+VersionInfoProductTextVersion={#VERSION}
 DisableDirPage=auto
 
 [Files]

configure.ac
@@ -3,8 +3,6 @@ dnl Process this file with autoconf to produce a configure script.
 AC_PREREQ(2.59)
 
 AC_INIT([poedit], [2.1], [help@poedit.net])
-PACKAGE_SHORT_VERSION=2.1
-AC_SUBST(PACKAGE_SHORT_VERSION)
 
 AC_CONFIG_AUX_DIR([admin])
 AC_CONFIG_MACRO_DIR([admin])

locales/Makefile.am
@@ -30,7 +30,7 @@ XARGS=xargs
 XGETTEXT_ARGS=-C -k_ -kwxGetTranslation -kwxTRANSLATE -kwxPLURAL:1,2 -F -j \
               --add-comments=TRANSLATORS \
               --from-code=UTF-8 \
-              --package-name=Poedit --package-version=$(PACKAGE_SHORT_VERSION) \
+              --package-name=Poedit --package-version=$(PACKAGE_VERSION) \
               --msgid-bugs-address=help@poedit.net
 
 # implicit rules

scripts/chversion.sh
@@ -25,33 +25,24 @@ replace_ver()
     $SED -e "s@$2@$3@g" --in-place $1
 }
 
-VER_FULL=$1
-VER_SHORT="`echo $VER_FULL | $SED -e 's/\(pre\|beta\|rc\)[0-9]//g'`"
-VER_WIN="`echo $VER_SHORT | tr '.' ','`"
+VERSION=$1
+VER_WIN="`echo $VERSION | tr '.' ','`"
 if [ `echo $VER_WIN | awk 'BEGIN{FS=","} {print NF}'` = 2 ] ; then
     VER_WIN="$VER_WIN,0"
 fi
 
 replace_ver win32/poedit.iss \
-            '\(#define VERSION_FULL *"\).*\("\)' "\1$VER_FULL\2"
-replace_ver win32/poedit.iss \
-            '\(#define VERSION *"\).*\("\)' "\1$VER_SHORT\2"
+            '\(#define VERSION *"\).*\("\)' "\1$VERSION\2"
 replace_ver win32/version.props \
-            '\(<PoeditVersion>\).*\(</PoeditVersion>\)' "\1$VER_SHORT\2"
+            '\(<PoeditVersion>\).*\(</PoeditVersion>\)' "\1$VERSION\2"
 replace_ver configure.ac \
-            '\(AC_INIT(\[poedit\], \[\)[^]]*\(\],.*\)' "\1$VER_FULL\2"
-replace_ver configure.ac \
-            '\(PACKAGE_SHORT_VERSION=\).*' "\1$VER_SHORT"
-replace_ver scripts/refresh-pot.sh \
-            '\(PACKAGE_SHORT_VERSION=\).*' "\1$VER_SHORT"
+            '\(AC_INIT(\[poedit\], \[\)[^]]*\(\],.*\)' "\1$VERSION\2"
 replace_ver src/version.h \
-            '\(POEDIT_VERSION *"\).*\("\)' "\1$VER_FULL\2"
-replace_ver src/version.h \
-            '\(POEDIT_VERSION_SHORT *"\).*\("\)' "\1$VER_SHORT\2"
+            '\(POEDIT_VERSION *"\).*\("\)' "\1$VERSION\2"
 replace_ver src/version.h \
             '\(POEDIT_VERSION_WIN *\).*' "\1$VER_WIN"
 replace_ver .travis.yml \
-            '\(file: poedit-\).*\(.tar.gz\)' "\1$VER_FULL\2"
+            '\(file: poedit-\).*\(.tar.gz\)' "\1$VERSION\2"
 replace_ver Poedit.xcodeproj/project.pbxproj \
-            '\(POEDIT_VERSION = \).*\(;\)' "\1$VER_FULL\2"
+            '\(POEDIT_VERSION = \).*\(;\)' "\1$VERSION\2"
 touch macos/Poedit-Info.plist

scripts/refresh-pot.sh
@@ -1,13 +1,13 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-PACKAGE_SHORT_VERSION=2.1
+PACKAGE_VERSION=2.1
 
 [ -n "${WXRC}" ] || WXRC=wxrc
 
 XGETTEXT_ARGS="-C -k_ -kwxGetTranslation -kwxTRANSLATE -kwxPLURAL:1,2 -F \
               --add-comments=TRANSLATORS \
               --from-code=UTF-8 \
-              --package-name=Poedit --package-version=${PACKAGE_SHORT_VERSION} \
+              --package-name=Poedit --package-version=${PACKAGE_VERSION} \
               --msgid-bugs-address=help@poedit.net"
 
 

src/edapp.cpp
@@ -310,8 +310,7 @@ wxString PoeditApp::GetAppBuildNumber() const
 
 bool PoeditApp::IsBetaVersion() const
 {
-    wxString v = GetAppVersion();
-    return v.Contains("beta") || v.Contains("rc");
+    return false;
 }
 
 bool PoeditApp::CheckForBetaUpdates() const

src/poedit.rc
@@ -59,12 +59,12 @@ BEGIN
             VALUE "Comments", "https://poedit.net"
             VALUE "CompanyName", "Vaclav Slavik\0"
             VALUE "FileDescription", "Poedit\0"
-            VALUE "FileVersion", POEDIT_VERSION_SHORT "\0"
+            VALUE "FileVersion", POEDIT_VERSION "\0"
             VALUE "InternalName", "Poedit\0"
             VALUE "OriginalFilename", "Poedit.exe\0"
             VALUE "LegalCopyright", "Copyright (C) 1999-2018 Vaclav Slavik\0"
             VALUE "ProductName", "Poedit\0"
-            VALUE "ProductVersion", POEDIT_VERSION_SHORT "\0"
+            VALUE "ProductVersion", POEDIT_VERSION "\0"
         END
     END
 

src/version.h
@@ -23,11 +23,10 @@
  *
  */
 
-#ifndef _VERSION_H_
-#define _VERSION_H_
+#ifndef Poedit_version_h
+#define Poedit_version_h
 
 #define POEDIT_VERSION           "2.1"
-#define POEDIT_VERSION_SHORT     "2.1"
 #define POEDIT_VERSION_WIN        2,1,0
 
-#endif // _VERSION_H_
+#endif // Poedit_version_h

commit e7fc79f170206181644d53f7c42adef9f1d3a221

Fix Inno Setup compilation breakage by b96300ab

win32/poedit.iss
@@ -41,8 +41,8 @@
 #endif
 
 [Setup]
-OutputBaseFilename=Poedit-{#VERSION_FULL}-setup
-OutputDir=win32\distrib-{#CONFIG}-{#VERSION_FULL}
+OutputBaseFilename=Poedit-{#VERSION}-setup
+OutputDir=win32\distrib-{#CONFIG}-{#VERSION}
 
 AppName=Poedit
 AppVerName=Poedit {#VERSION}