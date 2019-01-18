f00c9bbc4d2928f7380caa0cfdfaaeb03cc5a22e
Václav Slavík <vaclav@slavik.io>
2019-01-18 18:26:23 +0100

    Update copyright years

--- src/cat_sorting.cpp
+++ src/cat_sorting.cpp
@@ -1,7 +1,7 @@
 /*
  *  This file is part of Poedit (https://poedit.net)
  *
- *  Copyright (C) 1999-2018 Vaclav Slavik
+ *  Copyright (C) 1999-2019 Vaclav Slavik
  *  Copyright (C) 2005 Olivier Sannier
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
