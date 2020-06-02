vslavik/poedit

commit a9ace12bb579164e6b187869d17b91dc4db1a9fb

QA checks: narrower punctuation classification
Don't use u_ispunct() for checking whether trailing characters are
punctuation, because it is a POSIX compatibility function that checks
for punctuation _and_ symbols, and because some uninteresting
characters (e.g. @ and such) are classified as punctuation too.

Instead, limit ourselves to terminal punctuation (ending runs of text),
which is really the only kind we care about, plus quotation marks.

src/qa_checks.cpp
@@ -167,8 +167,8 @@ public:
 
         const UChar32 s_last = source.Last();
         const UChar32 t_last = translation.Last();
-        const bool s_punct = u_ispunct(s_last);
-        const bool t_punct = u_ispunct(t_last);
+        const bool s_punct = u_hasBinaryProperty(s_last, UCHAR_TERMINAL_PUNCTUATION) || u_hasBinaryProperty(s_last, UCHAR_QUOTATION_MARK);
+        const bool t_punct = u_hasBinaryProperty(t_last, UCHAR_TERMINAL_PUNCTUATION) || u_hasBinaryProperty(t_last, UCHAR_QUOTATION_MARK);
 
         if (u_getIntPropertyValue(s_last, UCHAR_BIDI_PAIRED_BRACKET_TYPE) == U_BPT_CLOSE ||
             u_getIntPropertyValue(t_last, UCHAR_BIDI_PAIRED_BRACKET_TYPE) == U_BPT_CLOSE)

commit 23e86f26dcfcb1f17aed145eaf113c2651cea77f

Fix ellipsis punctuation checks after a9ace12b
Apparently, U+2026 is not terminal punctuation in Unicode
classification, so the QA check for its correct use was accidentally
broken by a9ace12b.

src/qa_checks.cpp
@@ -167,8 +167,8 @@ public:
 
         const UChar32 s_last = source.Last();
         const UChar32 t_last = translation.Last();
-        const bool s_punct = u_hasBinaryProperty(s_last, UCHAR_TERMINAL_PUNCTUATION) || u_hasBinaryProperty(s_last, UCHAR_QUOTATION_MARK);
-        const bool t_punct = u_hasBinaryProperty(t_last, UCHAR_TERMINAL_PUNCTUATION) || u_hasBinaryProperty(t_last, UCHAR_QUOTATION_MARK);
+        const bool s_punct = IsPunctuation(s_last);
+        const bool t_punct = IsPunctuation(t_last);
 
         if (u_getIntPropertyValue(s_last, UCHAR_BIDI_PAIRED_BRACKET_TYPE) == U_BPT_CLOSE ||
             u_getIntPropertyValue(t_last, UCHAR_BIDI_PAIRED_BRACKET_TYPE) == U_BPT_CLOSE)
@@ -238,6 +238,13 @@ public:
     }
 
 private:
+    bool IsPunctuation(UChar32 c) const
+    {
+        return u_hasBinaryProperty(c, UCHAR_TERMINAL_PUNCTUATION) ||
+               u_hasBinaryProperty(c, UCHAR_QUOTATION_MARK) ||
+               c == L'…'; // somehow U+2026 ellipsis is not terminal punctuation
+    }
+
     bool IsEquivalent(UChar32 src, UChar32 trans) const
     {
         if (src == trans)