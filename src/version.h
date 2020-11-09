9c7dcff00f4f01c5d472e556f58131aa6e0b316a
Václav Slavík <vaclav@slavik.io>
2020-11-09 15:11:09 +0100

    Increase version to 2.4.2, update NEWS

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef Poedit_version_h
 #define Poedit_version_h
 
-#define POEDIT_VERSION           "2.4.1"
-#define POEDIT_VERSION_WIN        2,4,1
+#define POEDIT_VERSION           "2.4.2"
+#define POEDIT_VERSION_WIN        2,4,2
 
 #endif // Poedit_version_h
