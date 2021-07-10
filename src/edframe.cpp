vslavik/poedit

commit afad9c5ea338bd2d0a8e63f74db630f4402db93a

Use macOS 11 subtitles for file's description
Take advantage of title + subtitle distinction on macOS 11 and show file's
project name in the subtitle instead of appending it to the title.

src/edframe.cpp
@@ -2666,45 +2666,47 @@ void PoeditFrame::UpdateTitle()
 
     m_fileNamePartOfTitle.clear();
 
-    wxString title;
     auto fileName = GetFileName();
-    if ( !fileName.empty() )
+    
+    if (fileName.empty())
     {
-        wxFileName fn(GetFileName());
-        wxString fpath = fn.GetFullName();
+        SetTitle("Poedit");
+        return;
+    }
 
-        if (m_fileExistsOnDisk)
-            SetRepresentedFilename(fileName);
-        else
-            fpath += _(" (unsaved)");
+    wxString fpath = wxFileName(fileName).GetFullName();
 
-        if ( !m_catalog->Header().Project.empty() )
-        {
-            title.Printf(
+    if (m_fileExistsOnDisk)
+        SetRepresentedFilename(fileName);
+    else
+        fpath += _(" (unsaved)");
+
+    wxString title = fpath;
+    wxString subtitle = m_catalog->Header().Project;
+
 #ifdef __WXOSX__
-                L"%s — %s",
-#else
-                L"%s • %s",
-#endif
-                fpath, m_catalog->Header().Project);
-        }
-        else
-        {
-            title = fn.GetFullName();
-        }
-
-        m_fileNamePartOfTitle = title;
-
-#ifndef __WXOSX__
-        if ( IsModified() )
-            title += _(" (modified)");
-        title += " - Poedit";
-#endif
+  #if __MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_16
+    if (@available(macOS 11.0, *))
+    {
+        NSWindow *win = GetWXWindow();
+        win.subtitle = subtitle.empty() ? @"" : str::to_NS(subtitle);
     }
     else
+  #endif
+    if (!subtitle.empty())
     {
-        title = "Poedit";
+        title << MACOS_OR_OTHER(L" — ", L" • ");
+        title << subtitle;
     }
+#endif // __WXOSX__
+
+    m_fileNamePartOfTitle = title;
+
+#ifndef __WXOSX__
+    if ( IsModified() )
+        title += _(" (modified)");
+    title += " - Poedit";
+#endif
 
     SetTitle(title);
 }

commit 76ff2ff92c900db7bf517af4830918d8f7808541

Restore accidentally removed subtitle on non-Macs
This was broken by afad9c5 which unintentionally removed subtitle
handling from all non-macOS platforms.

src/edframe.cpp
@@ -2613,12 +2613,12 @@ void PoeditFrame::UpdateTitle()
     }
     else
   #endif
+#endif // __WXOSX__
     if (!subtitle.empty())
     {
         title << MACOS_OR_OTHER(L" — ", L" • ");
         title << subtitle;
     }
-#endif // __WXOSX__
 
     m_fileNamePartOfTitle = title;
 