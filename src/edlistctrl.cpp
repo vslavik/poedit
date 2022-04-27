vslavik/poedit

commit dc0cebea2968e904deed10f075fbe38302f10d75

Improve list update speed on Windows with hidden ID column
Continuation of a9d1b0c22d1fba81fca17bf4645d1e14e75e476c for when the
column is hidden.

See #340.

src/edlistctrl.cpp
@@ -688,6 +688,12 @@ void PoeditListCtrl::UpdateColumns()
         m_colID->SetWidth(wxCOL_WIDTH_AUTOSIZE);
         m_colID->SetWidth(m_colID->GetWidth());
     }
+    else
+    {
+        // workaround a wx bug where it calculates width of hidden columns
+        // see https://github.com/wxWidgets/wxWidgets/commit/560a81b913f23800e286d297d8cd38e72a207641
+        m_colID->SetWidth(0);
+    }
 
     SizeColumns();
 

commit 707a2b4f1f94cd2d2949cfbd6502e86459935af5

Add conditional for a workaround of a wxWidgets bug
dc0cebe introduced a workaround for a bug where wxWdigets calculated the
width of hidden data view columns. This bug was fixed in wxWidgets
3.1.3.

Add a conditional on the wxWidgets version to the workaround code so
that it can be removed once wxWidgets 3.2 is a prerequisite.

src/edlistctrl.cpp
@@ -177,10 +177,12 @@ public:
      */
     int GetWidth() const override
     {
+#if !wxCHECK_VERSION(3,1,3)
         // workaround a wx bug where it calculates width of hidden columns
         // see https://github.com/wxWidgets/wxWidgets/commit/560a81b913f23800e286d297d8cd38e72a207641
         if ( IsHidden() )
             return 0;
+#endif
 
         if ( fixed_width != wxCOL_WIDTH_DEFAULT )
             return fixed_width;