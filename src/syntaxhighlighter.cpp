3386be27d8cf2a0f74fc068444441c7136c3fa9c
Václav Slavík <vaclav@slavik.io>
2022-10-01 19:25:33 +0200

    Be more strict in matching Drupal :var placeholders
    64aefe03 was too eager match Drupal's :var placeholders, resulting in
    false positives on things like mailto:foo texts.
    
    Per Drupal docs, :var is used mostly for URLs passed to href and should
    always be quoted, i.e. in the form of href=":url" or href=':url'. Match
    it accordingly and only highlight quoted uses of :var.

--- src/syntaxhighlighter.cpp
+++ src/syntaxhighlighter.cpp
@@ -213,9 +213,10 @@ std::wregex RE_RUBY_FORMAT(LR"(%(\d+\$)?[-+ #0]{0,5}(\d+|\*)?(\.(\d+|\*))?(hh|ll
 std::wregex RE_COMMON_PLACEHOLDERS(
                     //
                     //           |             |               |
-                    LR"(%[\w.-]+%|%?\{[\w.-]+\}|\{\{[\w.-]+\}\}|[@%:][\w-]+)",
+                    LR"(%[\w.-]+%|%?\{[\w.-]+\}|\{\{[\w.-]+\}\}|[@%][\w-]+|":[\w-]+"|':[\w-]+')",
                     //      |    |      |      |        |      |      |
                     //      |           |               |             +--------- Drupal: non-terminated @var, %var or :var
+                    //                                                           (the last one only in href, as ":var" or ':var')
                     //      |           |               |                        !MUST be last in regex because of lack of termination character!
                     //      |           |               |
                     //      |           |               +----------------------- {{var}}
