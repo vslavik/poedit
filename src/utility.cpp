vslavik/poedit

commit 91c5f13257ffed9b80dc6744546f9cd25a2725ae

Fix gcc warnings

src/utility.cpp
@@ -302,8 +302,10 @@ bool TempOutputFileFor::ReplaceFile(const wxString& temp, const wxString& dest)
   #ifdef __UNIX__
     if (overwrite)
     {
-        chown(destPath, st.st_uid, st.st_gid);
-        chmod(destPath, st.st_mode);
+        if (!chown(destPath, st.st_uid, st.st_gid))
+            return false;
+        if (!chmod(destPath, st.st_mode))
+            return false;
     }
   #endif
 

src/titleless_window.h
@@ -29,7 +29,7 @@
 #include <wx/frame.h>
 #include <wx/dialog.h>
 
-class WXDLLIMPEXP_CORE wxButton;
+class WXDLLIMPEXP_FWD_CORE wxButton;
 
 
 /**

commit 0b911d38f528e90d9532e846d818208ec625bf0c

Fix chmod/chown return code verification logic (#772)
On Linux, TempOutputFileFor::ReplaceFile returns a wrong status, indicating
the replacement has failed when it has not. This leads to a misleading error
message displayed to user on every po file save.

The cause is in the verification of the return codes of chown/chmod.
chown and chmod return a non-zero integer value when an error occurs.
A non-zero value is considered as a boolean 'true'.

This commit fixes the logic error; It explicitly compares the returned
integer value to another integer to avoid confusion.

Fixes regression introduced in 91c5f13.

src/utility.cpp
@@ -302,9 +302,9 @@ bool TempOutputFileFor::ReplaceFile(const wxString& temp, const wxString& dest)
   #ifdef __UNIX__
     if (overwrite)
     {
-        if (!chown(destPath, st.st_uid, st.st_gid))
+        if (chown(destPath, st.st_uid, st.st_gid) != 0)
             return false;
-        if (!chmod(destPath, st.st_mode))
+        if (chmod(destPath, st.st_mode) != 0)
             return false;
     }
   #endif