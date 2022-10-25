838d04efa473dabdc965bab1fb6371c84ae9d9ee
Václav Slavík <vaclav@slavik.io>
2022-10-25 18:42:27 +0200

    Release version 3.2.1 (Unix only)
    Fixes regression introduced in 91c5f13.

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef Poedit_version_h
 #define Poedit_version_h
 
-#define POEDIT_VERSION           "3.2"
-#define POEDIT_VERSION_WIN        3,2,0
+#define POEDIT_VERSION           "3.2.1"
+#define POEDIT_VERSION_WIN        3,2,1
 
 #endif // Poedit_version_h
