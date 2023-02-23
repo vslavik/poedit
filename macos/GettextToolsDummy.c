3246f3f21b0da2ba2caaa94bffd53aac6dc31496
Václav Slavík <vaclav@slavik.io>
2023-02-23 10:38:16 +0100

    Update for Xcode 13.4
    Fix or silence new warnings.

--- macos/GettextToolsDummy.c
+++ macos/GettextToolsDummy.c
@@ -27,7 +27,7 @@
 // Xcode has issues with signing plugins/bundles that don't actually have
 // a binary in them. To work around this limitation, create a tiny dummy
 // plugin binary with a dummy function in it.
-int lets_keep_xcode_codesigning_happy_la_la_la()
+int lets_keep_xcode_codesigning_happy_la_la_la(void)
 {
     return 42;
 }
