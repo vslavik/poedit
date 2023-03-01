0af4120074e47bf96a8466f74b39364d942f5c94
Václav Slavík <vaclav@slavik.io>
2023-03-01 15:58:25 +0100

    Fix Crowdin files list appearance on macOS 11+

--- src/crowdin_gui.cpp
+++ src/crowdin_gui.cpp
@@ -341,6 +341,16 @@ public:
         SetRowHeight(PX(36));
     #endif
         SetMinSize(wxSize(PX(500), PX(200)));
+    #ifdef __WXOSX__
+      #if __MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_16
+        if (@available(macOS 11.0, *))
+        {
+            NSScrollView *scrollView = (NSScrollView*)GetHandle();
+            NSTableView *tableView = (NSTableView*)[scrollView documentView];
+            tableView.style = NSTableViewStyleFullWidth;
+        }
+      #endif
+    #endif
 
         auto renderer = new MultilineTextRenderer();
         auto column = new wxDataViewColumn(_("File"), renderer, 0, -1, wxALIGN_NOT, wxDATAVIEW_COL_RESIZABLE);
