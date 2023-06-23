vslavik/poedit

commit d358b9158ee815ee32dd1d716092b765505857f5

Require use of (PO)Catalog::Create()
Make sure that POCatalog ctor cannot be used directly and creation must
go either through Catalog::Create(), as is normally the case, or through
the newly added convenience wrapper POCatalog::Create().

This is because Create() calls PostCreation() which mustn't be skipped.

Fixes missing detection of source language in some operations.

src/cat_update.cpp
@@ -210,7 +210,7 @@ POCatalogPtr ExtractPOTFromSources(POCatalogPtr catalog, UpdateResultReason& rea
             {
                 try
                 {
-                    return std::make_shared<POCatalog>(potFile, Catalog::CreationFlag_IgnoreHeader);
+                    auto pot = POCatalog::Create(potFile, Catalog::CreationFlag_IgnoreHeader);
                 }
                 catch (...)
                 {
@@ -313,7 +313,7 @@ bool PerformUpdateFromPOTWithUI(wxWindow *parent,
 
     try
     {
-        auto pot = std::make_shared<POCatalog>(pot_file, Catalog::CreationFlag_IgnoreTranslations);
+        auto pot = POCatalog::Create(pot_file, Catalog::CreationFlag_IgnoreTranslations);
 
         // Silently fix duplicates because they are common in WP world:
         if (pot->HasDuplicateItems())

src/catalog.cpp
@@ -1134,7 +1134,7 @@ CatalogPtr Catalog::Create(Type type)
     {
         case Type::PO:
         case Type::POT:
-            return std::make_shared<POCatalog>(type);
+            return CatalogPtr(new POCatalog(type));
 
         case Type::XLIFF:
         case Type::JSON:
@@ -1155,7 +1155,7 @@ CatalogPtr Catalog::Create(const wxString& filename, int flags)
     CatalogPtr cat;
     if (POCatalog::CanLoadFile(ext))
     {
-        cat = std::make_shared<POCatalog>(filename, flags);
+        cat.reset(new POCatalog(filename, flags));
         flags = 0; // don't do the stuff below that is already handled by POCatalog's parser
     }
     else if (XLIFFCatalog::CanLoadFile(ext))

src/catalog.h
@@ -462,7 +462,7 @@ class Catalog
         };
 
         /// Default ctor. Creates empty catalog, you have to call Load.
-        static CatalogPtr Create(Type type = Type::PO);
+        static CatalogPtr Create(Type type);
 
         /**
             Ctor that loads the catalog from \a po_file with Load.

src/catalog_po.cpp
@@ -1686,7 +1686,7 @@ bool POCatalog::UpdateFromPOT(const wxString& pot_file, bool replace_header)
 {
     try
     {
-        POCatalogPtr pot = std::make_shared<POCatalog>(pot_file, CreationFlag_IgnoreTranslations);
+        POCatalogPtr pot = std::dynamic_pointer_cast<POCatalog>(Catalog::Create(pot_file, CreationFlag_IgnoreTranslations));
         return UpdateFromPOT(pot, replace_header);
     }
     catch (...) // FIXME
@@ -1725,7 +1725,7 @@ bool POCatalog::UpdateFromPOT(POCatalogPtr pot, bool replace_header)
 
 POCatalogPtr POCatalog::CreateFromPOT(POCatalogPtr pot)
 {
-    POCatalogPtr c = std::make_shared<POCatalog>();
+    POCatalogPtr c(new POCatalog(Type::PO));
     if (c->UpdateFromPOT(pot, /*replace_header=*/true))
         return c;
     else

src/catalog_po.h
@@ -148,17 +148,28 @@ typedef std::vector<POCatalogDeletedData> POCatalogDeletedDataArray;
 
 class POCatalog : public Catalog
 {
+protected:
+    /// Default ctor. Creates empty catalog, you have to call Load.
+    explicit POCatalog(Type type);
+
+    /// Ctor that loads the catalog from \a po_file with Load.
+    /// \a flags is CreationFlags combination.
+    explicit POCatalog(const wxString& po_file, int flags = 0);
+
 public:
     // Common wrapping values
     static const int NO_WRAPPING = -1;
     static const int DEFAULT_WRAPPING = -2;
 
-    /// Default ctor. Creates empty catalog, you have to call Load.
-    explicit POCatalog(Type type = Type::PO);
+    static POCatalogPtr Create(Type type)
+    {
+        return std::dynamic_pointer_cast<POCatalog>(Catalog::Create(type));
+    }
 
-    /// Ctor that loads the catalog from \a po_file with Load.
-    /// \a flags is CreationFlags combination.
-    explicit POCatalog(const wxString& po_file, int flags = 0);
+    static POCatalogPtr Create(const wxString& filename, int flags = 0)
+    {
+        return std::dynamic_pointer_cast<POCatalog>(Catalog::Create(filename, flags));
+    }
 
     ~POCatalog() {}
 
@@ -257,6 +268,7 @@ protected:
     bool m_hasPluralItems = false;
 
     friend class POLoadParser;
+    friend class Catalog;
 };
 
 

src/edapp.cpp
@@ -987,7 +987,7 @@ void PoeditApp::OnNewFromPOT(wxCommandEvent& event)
 
     try
     {
-        auto pot = std::make_shared<POCatalog>(dlg.GetPath(), Catalog::CreationFlag_IgnoreTranslations);
+        auto pot = POCatalog::Create(dlg.GetPath(), Catalog::CreationFlag_IgnoreTranslations);
 
         // Silently fix duplicates because they are common in WP world:
         if (pot->HasDuplicateItems())

src/edframe.cpp
@@ -1433,7 +1433,7 @@ void PoeditFrame::NewFromPOT(POCatalogPtr pot, Language language)
 
 void PoeditFrame::NewFromScratch()
 {
-    auto catalog = std::make_shared<POCatalog>();
+    auto catalog = POCatalog::Create(Catalog::Type::PO);
     catalog->CreateNewHeader();
 
     m_catalog = catalog;

src/manager.cpp
@@ -590,7 +590,7 @@ void ManagerFrame::OnUpdateProject(wxCommandEvent&)
                 {
                     Progress subtask(1, progress, 1);
 
-                    auto cat = std::make_shared<POCatalog>(f);
+                    auto cat = POCatalog::Create(f);
                     UpdateResultReason reason;
                     if (PerformUpdateFromSources(cat, reason))
                     {

commit 814af0dbef5c53acdfb41cb48763b508d16bd6d9

Fix broken ExtractPOTFromSources
This was accidentally broken by the d358b91 refactoring.

Fixes #799.

src/cat_update.cpp
@@ -211,6 +211,7 @@ POCatalogPtr ExtractPOTFromSources(POCatalogPtr catalog, UpdateResultReason& rea
                 try
                 {
                     auto pot = POCatalog::Create(potFile, Catalog::CreationFlag_IgnoreHeader);
+                    return pot;
                 }
                 catch (...)
                 {