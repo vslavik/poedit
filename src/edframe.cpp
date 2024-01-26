vslavik/poedit

commit 0ab10445e368e136f7464786a4208b3315afea87

Don't create empty window if loading a file fails

src/edframe.cpp
@@ -279,30 +279,12 @@ bool g_focusToText = false;
     }
     else
     {
-        // NB: duplicated in ReadCatalog()
-        try
-        {
-            auto cat = Catalog::Create(filename);
-            if (!cat || !cat->IsOk())
-                throw Exception(_("The file may be either corrupted or in a format not recognized by Poedit."));
-
-            f = new PoeditFrame();
-            f->Show(true);
-            f->ReadCatalog(cat);
-        }
-        catch (...)
-        {
-            wxMessageDialog dlg
-            (
-                nullptr,
-                _("The file cannot be opened."),
-                _("Invalid file"),
-                wxOK | wxICON_ERROR
-            );
-            dlg.SetExtendedMessage(DescribeCurrentException());
-            dlg.ShowModal();
+        auto cat = PreOpenFileWithErrorsUI(filename, nullptr);
+        if (!cat || !cat->IsOk())
             return nullptr;
-        }
+
+        f = new PoeditFrame();
+        f->ReadCatalog(cat);
     }
 
     f->Show(true);
@@ -952,14 +934,45 @@ void PoeditFrame::OnCloseCmd(wxCommandEvent&)
 void PoeditFrame::OpenFile(const wxString& filename, int lineno)
 {
     DoIfCanDiscardCurrentDoc([=]{
-        DoOpenFile(filename, lineno);
+        auto cat = PreOpenFileWithErrorsUI(filename, this);
+        if (cat && cat->IsOk())
+            DoOpenFile(cat, lineno);
     });
 }
 
 
-void PoeditFrame::DoOpenFile(const wxString& filename, int lineno)
+// FIXME: This is ugly API and exists only to support InvokingWindowProxy hacks in edapp.cpp;
+//        Once that is cleaned up to always open in a new window even on Windows, remove all this
+CatalogPtr PoeditFrame::PreOpenFileWithErrorsUI(const wxString& filename, wxWindow *parent)
 {
-    ReadCatalog(filename);
+    wxBusyCursor bcur;
+
+    try
+    {
+        auto cat = Catalog::Create(filename);
+        if (!cat || !cat->IsOk())
+            throw Exception(_("The file may be either corrupted or in a format not recognized by Poedit."));
+        return cat;
+    }
+    catch (...)
+    {
+        wxMessageDialog dlg
+        (
+            parent,
+            _("The file cannot be opened."),
+            _("Invalid file"),
+            wxOK | wxICON_ERROR
+        );
+        dlg.SetExtendedMessage(DescribeCurrentException());
+        dlg.ShowModal();
+        return nullptr;
+    }
+}
+
+
+void PoeditFrame::DoOpenFile(CatalogPtr cat, int lineno)
+{
+    ReadCatalog(cat);
 
     // HACK: make sure this is called *after* the delayed call in PoeditListCtrl::CatalogChanged
     if (m_list)
@@ -989,14 +1002,22 @@ void PoeditFrame::ReloadFileIfChanged()
             dlg->ShowWindowModalThenDo([this,dlg](int retval)
             {
                 if (retval == wxID_YES)
-                    ReadCatalog(m_catalog->GetFileName());
+                {
+                    auto cat = PreOpenFileWithErrorsUI(m_catalog->GetFileName(), this);
+                    if (cat && cat->IsOk())
+                        ReadCatalog(cat);
+                }
                 m_fileMonitor->StopRespondingToEvent();
             });
         }
         else
         {
             // file not modified in Poedit yet, so just reload it from the disk
-            ReadCatalog(m_catalog->GetFileName());
+            // TODO: Don't display errors in this case and just silently ignore the file; load the file
+            //       above before the prompt on background thread.
+            auto cat = PreOpenFileWithErrorsUI(m_catalog->GetFileName(), this);
+            if (cat && cat->IsOk())
+                ReadCatalog(cat);
             m_fileMonitor->StopRespondingToEvent();
         }
     }
@@ -1129,14 +1150,6 @@ void PoeditFrame::OnCloseWindow(wxCloseEvent& event)
         Destroy();
     }
 }
-
-
-#ifdef HAVE_HTTP_CLIENT
-void PoeditFrame::NewFromCrowdin(const wxString& filename)
-{
-    DoOpenFile(filename);
-}
-#endif
 
 
 void PoeditFrame::OnSave(wxCommandEvent& event)
@@ -2262,35 +2275,6 @@ void PoeditFrame::UpdateToTextCtrl(int flags)
     m_editingArea->UpdateToTextCtrl(item, flags);
 
     GetMenuBar()->Check(XRCID("menu_fuzzy"), item->IsFuzzy());
-}
-
-
-
-void PoeditFrame::ReadCatalog(const wxString& catalog)
-{
-    wxBusyCursor bcur;
-
-    // NB: duplicated in PoeditFrame::Create()
-    try
-    {
-        auto cat = Catalog::Create(catalog);
-        if (!cat || !cat->IsOk())
-            throw Exception(_("The file may be either corrupted or in a format not recognized by Poedit."));
-
-        ReadCatalog(cat);
-    }
-    catch (...)
-    {
-        wxMessageDialog dlg
-        (
-            this,
-            _("The file cannot be opened."),
-            _("Invalid file"),
-            wxOK | wxICON_ERROR
-        );
-        dlg.SetExtendedMessage(DescribeCurrentException());
-        dlg.ShowModal();
-    }
 }
 
 

src/edapp.cpp
@@ -641,8 +641,9 @@ void PoeditApp::OpenNewFile()
     WelcomeWindow::GetAndActivate();
 }
 
-void PoeditApp::OpenFiles(const wxArrayString& names, int lineno)
+int PoeditApp::OpenFiles(const wxArrayString& names, int lineno)
 {
+    int opened = 0;
     for ( auto name: names )
     {
         // MO files cannot be opened directly in Poedit (yet), but they are
@@ -660,9 +661,15 @@ void PoeditApp::OpenFiles(const wxArrayString& names, int lineno)
             continue;
         }
 
-        WelcomeWindow::HideActive();
-        PoeditFrame::Create(name, lineno);
+        if (PoeditFrame::Create(name, lineno))
+        {
+            opened++;
+        }
     }
+
+    if (opened)
+        WelcomeWindow::HideActive();
+    return opened;
 }
 
 
@@ -1026,11 +1033,21 @@ void PoeditApp::OnOpen(wxCommandEvent& event)
     wxArrayString paths;
     dlg.GetPaths(paths);
 
-    win.GetActionTarget()->DoOpenFile(paths.front());
-    paths.erase(paths.begin());
+    // This is special treatment for Windows' opening in existing window
+    {
+        auto cat = PoeditFrame::PreOpenFileWithErrorsUI(paths.front(), win.GetParentWindowIfAny());
+        if (cat && cat->IsOk())
+            win.GetActionTarget()->DoOpenFile(cat);
+        else if (paths.size() == 1)
+            win.NotifyWasAborted();
+    }
 
+    paths.erase(paths.begin());
     if (!paths.empty())
-        OpenFiles(paths);
+    {
+        if (OpenFiles(paths) == 0)
+            win.NotifyWasAborted();
+    }
 }
 
 
@@ -1045,8 +1062,15 @@ void PoeditApp::OnOpenFromCrowdin(wxCommandEvent& event)
     win.NotifyIsStarting();
     CrowdinOpenFile(win.GetParentWindowIfAny(), [=](int retval, wxString filename)
     {
-        if (retval == wxID_OK)
-            win.GetActionTarget()->NewFromCrowdin(filename);
+        if (retval != wxID_OK)
+        {
+            win.NotifyWasAborted();
+            return;
+        }
+
+        auto cat = PoeditFrame::PreOpenFileWithErrorsUI(filename, win.GetParentWindowIfAny());
+        if (cat && cat->IsOk())
+            win.GetActionTarget()->DoOpenFile(cat);
         else
             win.NotifyWasAborted();
     });
@@ -1061,8 +1085,12 @@ void PoeditApp::OnOpenHist(wxCommandEvent& event)
     if (!win.IsPerformingActionAllowed())
         return;
 
-    win.NotifyIsStarting();
-    win.GetActionTarget()->DoOpenFile(event.GetString());
+    auto cat = PoeditFrame::PreOpenFileWithErrorsUI(event.GetString(), win.GetParentWindowIfAny());
+    if (cat && cat->IsOk())
+    {
+        win.NotifyIsStarting();
+        win.GetActionTarget()->DoOpenFile(cat);
+    }
 }
 
 

src/edapp.h
@@ -62,8 +62,8 @@ class PoeditApp : public wxApp, public MenusManager
         bool IsBetaVersion() const;
         bool CheckForBetaUpdates() const;
 
-        // opens files in new frame
-        void OpenFiles(const wxArrayString& filenames, int lineno = 0);
+        // opens files in new frame, returns count of succesfully opened
+        int OpenFiles(const wxArrayString& filenames, int lineno = 0);
         // opens empty frame or catalogs manager
         void OpenNewFile();
 

src/edframe.h
@@ -83,8 +83,10 @@ class PoeditFrame : public PoeditFrameBase
         /// if there's unsaved document.
         void OpenFile(const wxString& filename, int lineno = 0);
 
+        static CatalogPtr PreOpenFileWithErrorsUI(const wxString& filename, wxWindow *parent);
+
         // Opens given file in this frame, without asking user
-        void DoOpenFile(const wxString& filename, int lineno = 0);
+        void DoOpenFile(CatalogPtr cat, int lineno = 0);
 
         // Re-read the file from disk if it changed externally
         void ReloadFileIfChanged();
@@ -104,8 +106,6 @@ class PoeditFrame : public PoeditFrameBase
 
         ~PoeditFrame();
 
-        /// Reads catalog, refreshes controls, takes ownership of catalog.
-        void ReadCatalog(const wxString& catalog);
         /// Reads catalog, refreshes controls, takes ownership of catalog.
         void ReadCatalog(const CatalogPtr& cat);
         /// Writes catalog.
@@ -154,9 +154,6 @@ class PoeditFrame : public PoeditFrameBase
 
         void NewFromScratch();
         void NewFromPOT(POCatalogPtr pot, Language language = Language());
-#ifdef HAVE_HTTP_CLIENT
-        void NewFromCrowdin(const wxString& filename);
-#endif
 
     protected:
         // Don't show help in status bar, it's not common to do these days:

commit a98484bb81b1f0bfe6b4f0865ee89eed6ff082f1

Fix showing autofix alert in some situations on macOS
In some specific situations, primarily when opening a file while Poedit
was already running, the FixDuplicatesIfPresent() would either not show
up at all or be assigned to the wrong window, because window-modal error
dialog was created with a not-yet-visible parent.

This was a regression introduced in 0ab1044.

src/edframe.cpp
@@ -286,6 +286,7 @@ bool g_focusToText = false;
             return nullptr;
 
         f = new PoeditFrame();
+        f->Show(true);
         f->ReadCatalog(cat);
     }
 
@@ -2365,6 +2366,8 @@ void PoeditFrame::ReadCatalog(const CatalogPtr& cat)
 
 void PoeditFrame::FixDuplicatesIfPresent()
 {
+    wxASSERT_MSG( IsShown(), "this method may show UI error, which requires the window to be visible" );
+
     auto cat = std::dynamic_pointer_cast<POCatalog>(m_catalog);
     if (!cat)
         return;