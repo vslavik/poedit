3d3ac4de71312dfb1572edd0eff87e3f36629bd4
Václav Slavík <vaclav@slavik.io>
2024-03-25 16:24:13 +0100

    Increase version to 3.5

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef Poedit_version_h
 #define Poedit_version_h
 
-#define POEDIT_VERSION           "3.4.2"
-#define POEDIT_VERSION_WIN        3,4,2
+#define POEDIT_VERSION           "3.5"
+#define POEDIT_VERSION_WIN        3,5,0
 
 #endif // Poedit_version_h
