6d98a1ba8873cfd9e57765684102c2663e3c78a1
Václav Slavík <vaclav@slavik.io>
2024-05-09 16:49:19 +0200

    Increase version to 3.4.4, update NEWS

--- src/version.h
+++ src/version.h
@@ -26,7 +26,7 @@
 #ifndef Poedit_version_h
 #define Poedit_version_h
 
-#define POEDIT_VERSION           "3.4.3"
-#define POEDIT_VERSION_WIN        3,4,3
+#define POEDIT_VERSION           "3.4.4"
+#define POEDIT_VERSION_WIN        3,4,4
 
 #endif // Poedit_version_h
