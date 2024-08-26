vslavik/poedit

commit 31a93ea584a1ce35f2b15188806d9b0053c67541

Remove no longer needed Crowdin workaround

scripts/update-translations-from-crowdin.sh
@@ -92,9 +92,7 @@ fixup_windows_rc_files()
 fixup_po_files()
 {
     for i in locales/*.po ; do
-        # Crowdin started producing bad Last-Translator header leading to
-        # warning: header field 'Last-Translator' still has the initial default value
-        $SED --in-place -e "s/Last-Translator: FULL NAME <EMAIL@ADDRESS>/Last-Translator: /g" "$i"
+        # nothing currently
     done
 }
 

commit 3cc2515b4b8eb572fc9a751d62a89899a30ef43a

Fix Crowdin script breakage in 31a93ea

scripts/update-translations-from-crowdin.sh
@@ -93,6 +93,7 @@ fixup_po_files()
 {
     for i in locales/*.po ; do
         # nothing currently
+        true
     done
 }
 