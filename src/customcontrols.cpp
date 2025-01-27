vslavik/poedit

commit 2b3b8735fa09769cf687f49d06ed623b10171fb4

Improve AutoWrappingText trimming

src/customcontrols.cpp
@@ -97,7 +97,10 @@ wxString WrapTextAtWidth(const wxString& text_, int width, Language lang, wxWind
 
     for (int32_t pos = iter->begin(); pos != iter->end(); pos = iter->next())
     {
+        if (pos == lineStart)
+            continue;
         auto substr = str::to_wx(text + lineStart, pos - lineStart);
+        substr.Trim();
 
         if (wnd->GetTextExtent(substr).x > width)
         {
@@ -140,8 +143,7 @@ wxString WrapTextAtWidth(const wxString& text_, int width, Language lang, wxWind
         out += previousSubstr;
     }
 
-    if (out.Last() == '\n')
-        out.RemoveLast();
+    out.Trim();
 
     return out;
 }

commit f08944cf431a2893692d15406a0e8a2a62e8a2aa

Fix auto wrapping of text with explict newlines
Previous changes to avoid including whitespace in width calculations
accidentally broke layout if the text contained intentional newlines.
They were previously handled correctly implicitly, before the change to
trim unnecessary whitespace, which now requires explicitness.

Fixes: 2b3b8735fa09 ("Improve AutoWrappingText trimming")

Fixes #878

src/customcontrols.cpp
@@ -129,6 +129,11 @@ wxString WrapTextAtWidth(const wxString& text_, int width, Language lang, wxWind
         else if (pos > 0 && text[pos-1] == '\n') // forced line feed
         {
             out += substr;
+            out += '\n';
+#ifdef BIDI_NEEDS_DIRECTION_ON_EACH_LINE
+            if (directionMark)
+                out += directionMark;
+#endif
             lineStart = pos;
             previousSubstr.clear();
         }