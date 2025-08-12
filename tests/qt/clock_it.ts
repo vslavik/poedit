67877f2931785c0682555370ae9040a6d57cd4ac
Václav Slavík <vaclav@slavik.io>
2025-08-12 17:05:29 +0200

    Add Qt example files

---
+++ tests/qt/clock_it.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it" sourcelanguage="en">
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../Main.qml" line="15"/>
+        <source>Digital Clock</source>
+        <translation>Orologio digitale</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../Main.qml" line="33"/>
+        <source>%n second(s)</source>
+        <comment>seconds</comment>
+        <translation>
+            <numerusform>%n secondo</numerusform>
+            <numerusform>%n secondi</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../Main.qml" line="43"/>
+        <source>Locale: %1</source>
+        <translation>Impostazioni locali: %1</translation>
+    </message>
+</context>
+</TS>
