bfd1f423bcee85008c7db478f2139e3d45af338a
Václav Slavík <vaclav@slavik.io>
2026-05-15 09:43:48 +0200

    Xcode: more resilient downloads from ftpmirror.gnu.org
    The mirror system is sometimes a bit flaky, so retry downloads.

--- deps/generate_build_ninja.py
+++ deps/generate_build_ninja.py
@@ -102,7 +102,7 @@ with open('build.ninja', 'w') as buildfile:
     n.rule('download',
            description='Downloading $url...',
            pool='console',
-           command='curl -L -o $out $url && test "$sha256" = `shasum -a256 $out | cut -f1 -d" "`')
+           command='curl --fail --location --retry 5 --retry-all-errors -o $out $url && test "$sha256" = `shasum -a256 $out | cut -f1 -d" "`')
 
     targets = []
 
